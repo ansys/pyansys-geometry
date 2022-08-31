bf0b6a7b34a2f645358793a7cd55881053380509
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2022-08-31 12:39:04 +0200

    Add 3D vector

---
+++ src/ansys/geometry/core/primitives/vector3D.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+
+class Vector3D(np.ndarray):
+    """Subclassing ndarray and casting an existing ndarray as a given subclass
+    View casting is the standard ndarray mechanism by which you take an ndarray of any subclass,
+    and return a view of the array as another (specified) subclass
+    The first is the use of the ndarray.__new__ method for the main work of object initialization,
+    rather then the more usual __init__ method. The second is
+    the use of the __array_finalize__ method to allow subclasses to clean up
+    after the creation of views and new instances from templates."""
+
+    def __new__(cls, input_array):
+
+        vector = np.asarray(input_array).view(cls)
+
+        if len(vector) != 3:
+            raise ValueError("Vector3D must have three coordinates.")
+
+        return vector
+
+    @property
+    def norm(self):
+        norm = np.linalg.norm(self)
+        if norm == 0:
+            norm = np.finfo(self.dtype).eps
+        return norm
+
+    def normalize(self):
+        return self / self.norm
+
+    def cross(self, v: "Vector3D") -> "Vector3D":
+        return Vector3D(np.cross(self, v))
+
+    def dot(self, v: "Vector3D") -> float:
+        return np.dot(self, v)
