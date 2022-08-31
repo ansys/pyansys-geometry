4f719e1e4c49e06ad86ca1a566d25a1e95ed8c09
RobPasMue <roberto.pastormuela@ansys.com>
2022-08-31 10:13:16 +0200

    Create line properties

--- src/ansys/geometry/core/sketch/line_sketch.py
+++ src/ansys/geometry/core/sketch/line_sketch.py
@@ -20,3 +20,27 @@ class LineSketch(SketchCurve):
         """Constructor method for ``LineSketch``."""
         self._point_1 = point_1
         self._point_2 = point_2
+
+    @property
+    def point_1(self):
+        """Returns the start of the line segment."""
+        return self._point_1
+
+    @point_1.setter
+    def point_1(self, point_1):
+        """Set the start of the line segment."""
+        if not isinstance(point_1, Point3D):
+            raise ValueError("The parameter 'point_1' should be a Point3D object.")
+        self._point_1 = point_1
+
+    @property
+    def point_2(self):
+        """Returns the end of the line segment."""
+        return self._point_2
+
+    @point_2.setter
+    def point_2(self, point_2):
+        """Set the end of the line segment."""
+        if not isinstance(point_2, Point3D):
+            raise ValueError("The parameter 'point_2' should be a Point3D object.")
+        self._point_1 = point_2
