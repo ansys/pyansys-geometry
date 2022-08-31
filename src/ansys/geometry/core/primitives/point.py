e535c29c70517280fee6dc65e7b9a89a6b595c47
RobPasMue <roberto.pastormuela@ansys.com>
2022-08-31 12:30:54 +0200

    Consolidate Point3D implementation

--- src/ansys/geometry/core/primitives/point.py
+++ src/ansys/geometry/core/primitives/point.py
@@ -7,23 +7,18 @@ class Point3D(np.ndarray):
     Provides Point3D geometry primitive representation.
     """
 
-    def __new__(cls, input_array):
-        obj = np.asarray(input_array).view(cls)
-
+    def __new__(cls, *args):
+        
+        if (len(args) == 1) or (len(args) == 3):
+            obj = np.asarray(args).view(cls)
+            
         if len(obj) != 3:
             raise ValueError("Point3D must have three coordinates.")
 
-        if not all(isinstance(value, (int, float)) for value in obj):
+        if not all(isinstance(value, (int, float)) for value in args):
             raise ValueError("The parameters of 'input_array' should be integer or float.")
 
         return obj
-
-    def __init__(self, x: float, y: float, z: float):
-        """Constructor method for ``Point3D``."""
-        if not all(isinstance(value, (int, float)) for value in [x, y, z]):
-            raise ValueError("The parameters 'x', 'y' and 'z' should be integer or float.")
-
-        self = np.asarray([x, y, z])
 
     @property
     def x(self):
@@ -60,3 +55,14 @@ class Point3D(np.ndarray):
         if not isinstance(z, (int, float)):
             raise ValueError("The parameter 'z' should be a float or an integer value.")
         self[2] = z
+
+    def __eq__(self, __o: object) -> bool:
+        """Equals operator for ``Point3D``."""
+        if not isinstance(__o, Point3D):
+            raise ValueError(f"Comparison of {self} against {__o} is not possible.")
+
+        return np.array_equal(self, __o)
+    
+    def __ne__(self, other) -> bool:
+        """Not equals operator for ``Point3D``."""
+        return not self.__eq__(other)
