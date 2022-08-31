11857af03102190e755614d582ebfc511e8f9fa0
RobPasMue <roberto.pastormuela@ansys.com>
2022-08-31 10:12:48 +0200

    Pre-commit

--- src/ansys/geometry/core/sketch/__init__.py
+++ src/ansys/geometry/core/sketch/__init__.py
@@ -4,5 +4,4 @@ from ansys.geometry.core.sketch.circle_sketch import CircleSketch
 from ansys.geometry.core.sketch.line_sketch import LineSketch
 from ansys.geometry.core.sketch.sketch import Sketch
 
-
 __all__ = ["Sketch", "CircleSketch", "LineSketch"]
