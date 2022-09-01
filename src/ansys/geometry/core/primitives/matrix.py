d3287bc6e9b72e50bcf01736c48c928338ef7a80
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2022-09-01 14:34:39 +0200

    Add inverse matrix

--- src/ansys/geometry/core/primitives/matrix.py
+++ src/ansys/geometry/core/primitives/matrix.py
@@ -1,61 +1,47 @@
 import numpy as np
 
 
-class Matrix3:
+class Matrix3(np.ndarray):
     def __new__(cls, input):
         """Constructor for ``Point3D``."""
 
         matrix = np.asarray(input).view(cls)
 
-        if matrix is None or matrix.ndim != 2 and matrix.shape != (3, 3):
-            raise ValueError("Matrix3 shouldonly be a 2D array of shape (3,3).")
+        if matrix is None or matrix.ndim != 2 or matrix.shape != (3, 3):
+            raise ValueError("Matrix3 should only be a 2D array of shape (3,3).")
 
-        if not np.issubdtype(matrix.dtype, np.number) or not isinstance(input, (np.ndarray)):
+        if not np.issubdtype(matrix.dtype, np.number) or not isinstance(matrix, (np.ndarray)):
             raise ValueError("The input parameters should be integer or float.")
 
         return matrix
 
-    def __copy__(self):
-        m_copy = self.matrix
-        return m_copy
-
-    @property
-    def matrix(self):
-        return self.matrix
-
-    @matrix.setter
-    def matrix(self, matrix):
-        self = matrix
+    def inverse(self):
+        det = np.linalg.det(self)
+        if det == 0:
+            raise ValueError("The determinent of matrix is zero, cannot be inversed")
+        return np.linalg.inv(self)
 
 
-# a b c d
-# e f g h
-# i j k l
-# m n o p
-
-
-class Matrix4:
+class Matrix4(np.ndarray):
     def __new__(cls, input):
         """Constructor for ``Point3D``."""
 
         matrix = np.asarray(input).view(cls)
 
-        if matrix is None or matrix.ndim != 2 and matrix.shape != (3, 3):
+        if matrix is None or matrix.ndim != 2 or matrix.shape != (4, 4):
             raise ValueError("Matrix3 shouldonly be a 2D array of shape (3,3).")
 
-        if not np.issubdtype(matrix.dtype, np.number) or not isinstance(input, (np.ndarray)):
+        if not np.issubdtype(matrix.dtype, np.number) or not isinstance(matrix, (np.ndarray)):
             raise ValueError("The input parameters should be integer or float.")
 
         return matrix
 
-    def __copy__(self):
-        m_copy = self.matrix
-        return m_copy
+    def inverse(self):
+        det = np.linalg.det(self)
+        if det == 0:
+            raise ValueError("The determinent of matrix is zero, cannot be inversed")
+        return np.linalg.inv(self)
 
-    @property
-    def matrix(self):
-        return self.matrix
 
-    @matrix.setter
-    def matrix(self, matrix):
-        self = matrix
+# from ansys.geometry.core.primitives import Matrix3
+# a = Matrix3([[1,2,3],[2,5,6],[2,5,10]])
