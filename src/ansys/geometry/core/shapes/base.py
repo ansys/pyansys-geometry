3c0faf28fc9aa78f73fc0d3e02561b8e02d8a13e
Jorge Martínez Garrido <jorge.martinezgarrido@ansys.com>
2022-09-05 14:16:21 +0200

    Refactor base.py

--- src/ansys/geometry/core/shapes/base.py
+++ src/ansys/geometry/core/shapes/base.py
@@ -1,70 +1,106 @@
 """``SketchCurve`` class module."""
 
-from typing import Sequence
-
-from ansys.geometry.core.math.point import Point2D
-from ansys.geometry.core.typing import RealSequence
+from ansys.geometry.core.math.point import Point3D
+from ansys.geometry.core.math.vector import Vector3D
 
 
-class SketchCurve:
-    """Provides base sketch object class all sketch objects."""
+class BaseShape:
+    """Provides mother class for modeling base shape objects."""
 
-    def __init__(self, points: Sequence[Point2D], origin: Point2D):
+    def __init__(self, origin: Point3D, dir_1: Vector3D([1, 0, 0]), dir_2: Vector3D([0, 1, 0])):
         """Initializes the sketch curve from its points.
 
         Parameters
         ----------
-        points : Sequence[Point2D]
-            A list of points defining the sketch curve.
-        origin : Point2D
-            A ``Point2D`` representing the origin of the ellipse.
+        origin : Point3D
+            A ``Point3D`` representing the origin of the shape.
+        dir_1 : Vector3D
+            A :class:``Vector3D`` representing the first fundamental direction
+            of the reference plane where the sape is contained.
+        dir_2 : Vector3D
+            A :class:``Vector3D`` representing the second fundamental direction
+            of the reference plane where the sape is contained.
 
         """
-        self._points = points
+        # TODO: assign a reference frame to the base shape
+        # self._frame = Frame.from_origin_and_vectors(origin, dir_1, dir_2)
+        # self._plane = self._frame.plane
+        # self._origin = self._frame.origin
+
+        # TODO: deprecate in favor of reference frame
+        if dir_1.dot(dir_2) != 0:
+            raise ValueError("Reference vectors are not orthogonal.")
+        self._i, self._j = dir_1.normalize(), dir_2.normalize()
+        self._k = self.i.cross(self._j)
         self._origin = origin
 
     @property
-    def points(self) -> Sequence[Point2D]:
-        """Return a list of ``Point2D`` instances defining the sketch.
-
-        Returns
-        -------
-        Sequence[Point2D]
-            A list of points defining the sketch curve.
-
-        """
-        return self._points
+    def i(self) -> Vector3D:
+        """The fundamental vector along the first axis of the reference frame."""
+        return self._i
 
     @property
-    def origin(self) -> Point2D:
-        """Return the origin of the sketch.
-
-        Returns
-        -------
-        Point2D
-            A ``Point2D`` instance representing the center of the sketch curve
-
-        """
-        return self._origin
+    def j(self) -> Vector3D:
+        """The fundamental vector along the second axis of the reference frame."""
+        return self._j
 
     @property
-    def x_coords(self) -> RealSequence:
-        """Returns the x-coordinates of the sketch curve.
-
-        Returns
-        -------
-        RealSequence
-
-        """
-        return [point.x for point in self.points]
+    def k(self) -> Vector3D:
+        """The fundamental vector along the third axis of the reference frame."""
+        return self._k
 
     @property
-    def y_coords(self) -> RealSequence:
-        """Returns the y-coordinates of the sketch curve.
+    def origin(self) -> Vector3D:
+        """The origin of the reference frame."""
+        return origin
+
+    def points(self, num_points=100) -> list[Point3D]:
+        """Returns al list containing all the points belonging to the sape.
+
+        Parameters
+        ----------
+        num_points : int
+            Desired number of points belonging to the shape.
 
         Returns
         -------
-        RealSequence
+        list[Point3D]
+            A list of points representing the shape.
+        """
+        return self.frame.from_local_to_global @ self.local_points
+
+    @property
+    def x_coordinates(sefl) -> list[real]:
+        """Return all the x coordinates for the points of the shape.
+
+        Returns
+        -------
+        list[real]
+            A list containing the values for the x-coordinates of the shape.
 
         """
-        return [point.y for point in self.points]
+        return [point[0] for point in self._points]
+
+    @property
+    def y_coordinates(sefl) -> list[real]:
+        """Return all the y coordinates for the points of the shape.
+
+        Returns
+        -------
+        list[real]
+            A list containing the values for the y-coordinates of the shape.
+
+        """
+        return [point[1] for point in self._points]
+
+    @property
+    def z_coordinates(sefl) -> list[real]:
+        """Return all the y coordinates for the points of the shape.
+
+        Returns
+        -------
+        list[real]
+            A list containing the values for the z-coordinates of the shape.
+
+        """
+        return [point[2] for point in self._points]
