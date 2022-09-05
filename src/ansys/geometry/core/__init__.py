5df777a4ccab927c727f0456d54f5e6320e7acd1
RobPasMue <roberto.pastormuela@ansys.com>
2022-09-05 13:59:09 +0200

    Including version docstring

--- src/ansys/geometry/core/__init__.py
+++ src/ansys/geometry/core/__init__.py
@@ -13,6 +13,7 @@ except ModuleNotFoundError:
     import importlib_metadata
 
 __version__ = importlib_metadata.version(__name__.replace(".", "-"))
+"""The installed version of PyGeometry"""
 
 
 # Units
