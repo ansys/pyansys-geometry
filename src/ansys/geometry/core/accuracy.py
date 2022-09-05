6dca27448de0c0ea888f42648d555659e99557f9
Chad Queen <chadqueen82@gmail.com>
2022-09-05 15:11:45 -0400

    Accuracy model started with basic tolerance comparisons

---
+++ src/ansys/geometry/core/accuracy.py
@@ -0,0 +1,55 @@
+"""``Accuracy`` class module."""
+
+LENGTH_ACCURACY = 1e-8
+ANGLE_ACCURACY = 1e-6
+
+
+import math
+
+
+class Accuracy:
+    """
+    Provides accuracy evaluations for actions such as equivalency evaluations.
+    """
+
+    def length_is_zero(length: float) -> bool:
+        """Returns True if length is within length accuracy of exact zero."""
+        return length <= LENGTH_ACCURACY and length >= -LENGTH_ACCURACY
+
+    def length_is_negative(length: float) -> bool:
+        """Returns True if length is below a negative length accuracy."""
+        return length < -LENGTH_ACCURACY
+
+    def length_is_positive(length: float) -> bool:
+        """Returns True if length is above a positive length accuracy."""
+        return length > LENGTH_ACCURACY
+
+    def angle_is_zero(angle: float) -> bool:
+        """Returns True if length is within angle accuracy of exact zero."""
+        return abs(angle) < ANGLE_ACCURACY
+
+    def angle_is_negative(angle: float) -> bool:
+        """Returns True if angle is below a negative angle accuracy."""
+        return angle <= -ANGLE_ACCURACY
+
+    def angle_is_positive(angle: float) -> bool:
+        """Returns True if angle is above a positive angle accuracy."""
+        return angle >= ANGLE_ACCURACY
+
+    def is_within_tolerance(
+        a: float, b: float, relative_tolerance: float, absolute_tolerance: float
+    ) -> bool:
+        # The code doesn't work for comparing infinity and non-infinite numbers!
+        a_is_infinite = math.isinf(a)
+        b_is_infinite = math.isinf(b)
+
+        if a_is_infinite or b_is_infinite:
+            if (a_is_infinite and a > 0) ^ (b_is_infinite and b > 0):
+                return False
+            if (a_is_infinite and a < 0) ^ (b_is_infinite and b < 0):
+                return False
+
+        difference = abs(a - b)
+        return difference < absolute_tolerance or difference <= relative_tolerance * 0.5 * abs(
+            a + b
+        )
