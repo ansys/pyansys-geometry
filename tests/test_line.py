1896eeeb095a501e0af4e87a15973ebe97ebca15
RobPasMue <roberto.pastormuela@ansys.com>
2022-09-05 10:55:20 +0200

    Pre-commit checks

--- tests/test_line.py
+++ tests/test_line.py
@@ -5,11 +5,14 @@ from ansys.geometry.core.sketch.line import Line, Segment
 def test_create_line():
     """Simple test to create a ``Line``."""
 
+
 def test_create_segment():
     """Simple test to create a ``Segment``."""
 
+
 def test_errors_line():
     """Check errors when handling a ``Line``."""
+
 
 def test_errors_segment():
     """Check errors when handling a ``Line``."""
