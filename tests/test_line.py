6ae1ab2fc9dd38545a21f02e9f943a34b74f5223
RobPasMue <roberto.pastormuela@ansys.com>
2022-09-05 10:55:03 +0200

    Adding test_line module

---
+++ tests/test_line.py
@@ -0,0 +1,15 @@
+from ansys.geometry.core.primitives import Point3D
+from ansys.geometry.core.sketch.line import Line, Segment
+
+
+def test_create_line():
+    """Simple test to create a ``Line``."""
+
+def test_create_segment():
+    """Simple test to create a ``Segment``."""
+
+def test_errors_line():
+    """Check errors when handling a ``Line``."""
+
+def test_errors_segment():
+    """Check errors when handling a ``Line``."""
