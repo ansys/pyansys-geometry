3bafdb4dd225e85ea50ccaa09aba63db43a262f5
RobPasMue <roberto.pastormuela@ansys.com>
2022-09-06 11:31:37 +0200

    Adding QuantityVectorXD to __init__

--- src/ansys/geometry/core/primitives/__init__.py
+++ src/ansys/geometry/core/primitives/__init__.py
@@ -1,6 +1,23 @@
 """PyGeometry primitives subpackage."""
 from ansys.geometry.core.primitives.cylinder import Cylinder
 from ansys.geometry.core.primitives.point import Point2D, Point3D
-from ansys.geometry.core.primitives.vector import UnitVector2D, UnitVector3D, Vector2D, Vector3D
+from ansys.geometry.core.primitives.vector import (
+    QuantityVector2D,
+    QuantityVector3D,
+    UnitVector2D,
+    UnitVector3D,
+    Vector2D,
+    Vector3D,
+)
 
-__all__ = ["Cylinder", "Point2D", "Point3D", "UnitVector2D", "UnitVector3D", "Vector2D", "Vector3D"]
+__all__ = [
+    "Cylinder",
+    "Point2D",
+    "Point3D",
+    "QuantityVector2D",
+    "QuantityVector3D",
+    "UnitVector2D",
+    "UnitVector3D",
+    "Vector2D",
+    "Vector3D",
+]
