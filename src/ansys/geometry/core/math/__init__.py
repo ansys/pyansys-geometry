3bb9fd33c828b0e6ad33283b2e1fc182609303c1
RobPasMue <roberto.pastormuela@ansys.com>
2022-09-06 20:50:28 +0200

    Pre-commit checks

--- src/ansys/geometry/core/math/__init__.py
+++ src/ansys/geometry/core/math/__init__.py
@@ -1,7 +1,14 @@
 """PyGeometry math subpackage."""
 from ansys.geometry.core.math.matrix import Matrix, Matrix33, Matrix44
 from ansys.geometry.core.math.point import Point2D, Point3D
-from ansys.geometry.core.math.vector import QuantityVector2D, QuantityVector3D, UnitVector2D, UnitVector3D, Vector2D, Vector3D
+from ansys.geometry.core.math.vector import (
+    QuantityVector2D,
+    QuantityVector3D,
+    UnitVector2D,
+    UnitVector3D,
+    Vector2D,
+    Vector3D,
+)
 
 __all__ = [
     "Matrix",
