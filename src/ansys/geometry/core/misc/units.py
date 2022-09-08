0382bbe6dbf58ecabac7240c3218bcc83acdcb24
RobPasMue <roberto.pastormuela@ansys.com>
2022-09-08 13:34:37 +0200

    Basic PhysicalQuantity class implementation

--- src/ansys/geometry/core/misc/units.py
+++ src/ansys/geometry/core/misc/units.py
@@ -1,8 +1,13 @@
 """``Units`` module for PyGeometry."""
 
-from pint import Unit, UnitRegistry
+from pint import Quantity, Unit, UnitRegistry
 
-from ansys.geometry.core.misc.checks import check_is_pint_unit, check_pint_unit_compatibility
+from ansys.geometry.core.misc.checks import (
+    check_is_float_int,
+    check_is_pint_unit,
+    check_pint_unit_compatibility,
+)
+from ansys.geometry.core.typing import Real
 
 UNITS = UnitRegistry()
 """Unit manager."""
@@ -25,7 +30,9 @@ class PhysicalQuantity:
 
     def __init__(self, unit: Unit):
         """Constructor for ``PhysicalQuantity``."""
+        check_is_pint_unit(unit, "unit")
         self._unit = unit
+        _, self._base_unit = UNITS.get_base_units(unit)
 
     @property
     def unit(self) -> Unit:
@@ -36,10 +43,25 @@ class PhysicalQuantity:
     def unit(self, unit: Unit) -> None:
         """Sets the unit of the object."""
         check_is_pint_unit(unit, "unit")
-        check_pint_unit_compatibility(unit, UNIT_LENGTH)
+        check_pint_unit_compatibility(unit, self._base_unit)
         self._unit = unit
 
     @property
     def base_unit(self) -> Unit:
         """Returns the base unit of the object."""
         return self._base_unit
+
+    def __get_quantity(self, input: Real) -> Quantity:
+        """Returns input value as a ~:class:`pint.Quantity`.
+
+        Parameters
+        ----------
+        input : Real
+            The number to be expressed as a quantity
+
+        Returns
+        -------
+        Quantity
+            The physical quantity the number represents.
+        """
+        return (input * self.base_unit).ito(self.unit)
