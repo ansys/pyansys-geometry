9ea3308ada9acc66328c9be9891dc7aff4d0c4b4
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2022-09-12 11:01:57 +0200

    Add Units to rotation

--- src/ansys/geometry/core/transformation.py
+++ src/ansys/geometry/core/transformation.py
@@ -1,15 +1,23 @@
-from typing import Union
+from typing import Optional, Union
 
 import numpy as np
+from pint import Unit
 from scipy.spatial.transform import Rotation as spatial_rotation
 
 from ansys.geometry.core.math import Vector
-from ansys.geometry.core.misc.checks import check_ndarray_is_float_int
+from ansys.geometry.core.misc import UNIT_ANGLE, check_ndarray_is_float_int
+from ansys.geometry.core.misc.units import PhysicalQuantity
 from ansys.geometry.core.typing import Real
 
 
-class Rotation(np.ndarray):
-    def __new__(cls, input: object, angle: Union[Real, np.ndarray], axis: str) -> object:
+class Rotation(np.ndarray, PhysicalQuantity):
+    def __new__(
+        cls,
+        input: object,
+        angle: Union[Real, np.ndarray],
+        axis: str,
+        unit: Optional[Unit] = UNIT_ANGLE,
+    ) -> object:
         """Constructor for ``Rotation``."""
         obj = np.asarray(input).view(cls)
         check_ndarray_is_float_int(obj)
@@ -24,6 +32,7 @@ class Rotation(np.ndarray):
             raise ValueError("Axis and angle are not matching")
         if ang.ndim >= 2 and len(axis) != ang.shape[1]:
             raise ValueError("Axis and angle are not matching")
+        super().__init__(cls, unit, expected_dimensions=UNIT_ANGLE)
         obj._angle = ang
         obj._axis = axis
         obj_type = type(input)
@@ -35,6 +44,7 @@ class Rotation(np.ndarray):
 class Translation(np.ndarray):
     def __new__(cls, input: object, v: Vector):
         obj = np.asarray(input).view(cls)
+        obj = np.append(obj, [1])
         if v._is_3d == True:
             translate = np.array(
                 [
@@ -52,7 +62,7 @@ class Translation(np.ndarray):
                     [0, 0, 1],
                 ]
             )
-        return np.matmul(translate, obj)
+        return np.matmul(translate, obj)[:-1]
 
 
 class Scale(np.ndarray):
--- tests/test_transformation.py
+++ tests/test_transformation.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from ansys.geometry.core.math import Matrix33, Vector
+from ansys.geometry.core.misc.units import UNITS
 from ansys.geometry.core.transformation import Rotation, Translation
 
 DOUBLE_EPS = np.finfo(float).eps * 2
@@ -13,6 +14,12 @@ def test_transformation_rotation():
     assert rot_x.x - test_vector.x <= DOUBLE_EPS
     assert rot_x.y - test_vector.y <= DOUBLE_EPS
     assert rot_x.z - test_vector.z <= DOUBLE_EPS
+
+    # Rotation along x axis in degree
+    rot_x_degree = Rotation(v_1, 90, "x", unit=UNITS.degree)
+    assert rot_x_degree.x - test_vector.x <= DOUBLE_EPS
+    assert rot_x_degree.y - test_vector.y <= DOUBLE_EPS
+    assert rot_x_degree.z - test_vector.z <= DOUBLE_EPS
     m = Matrix33()
     rot_x = Rotation(m, [np.pi / 2, np.pi / 2], "xy")
     test = np.array([[0, 0, -1], [1.0, 0, 0], [0.0, -1, 0]])
