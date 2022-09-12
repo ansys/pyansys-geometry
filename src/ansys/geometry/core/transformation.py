bd86ac86e28189408fcc42e09add1f6e374db3ae
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2022-09-12 10:00:24 +0200

    Add translation

--- src/ansys/geometry/core/transformation.py
+++ src/ansys/geometry/core/transformation.py
@@ -3,8 +3,7 @@ from typing import Union
 import numpy as np
 from scipy.spatial.transform import Rotation as spatial_rotation
 
-from ansys.geometry.core.math import Vector3D
-from ansys.geometry.core.math.vector import Vector2D
+from ansys.geometry.core.math import Vector
 from ansys.geometry.core.misc.checks import check_ndarray_is_float_int
 from ansys.geometry.core.typing import Real
 
@@ -15,7 +14,6 @@ class Rotation(np.ndarray):
         obj = np.asarray(input).view(cls)
         check_ndarray_is_float_int(obj)
         ang = np.asarray(angle).view(cls)
-        obj_type = type(input)
         if len(axis) > 3:
             raise ValueError(
                 f"Expected axis specification to be a string of up to 3 characters, got{axis}"
@@ -28,27 +26,37 @@ class Rotation(np.ndarray):
             raise ValueError("Axis and angle are not matching")
         obj._angle = ang
         obj._axis = axis
+        obj_type = type(input)
         rotated_obj = spatial_rotation.from_euler(obj._axis, obj._angle)
         obj_rot = rotated_obj.apply(obj)
-        return obj_rot.view(obj_type)
+        return obj_type(obj_rot)
 
 
 class Translation(np.ndarray):
-    def __new__(cls, input: object, v: Union[Vector2D, Vector3D]):
+    def __new__(cls, input: object, v: Vector):
         obj = np.asarray(input).view(cls)
-        translate = np.array(
-            [
-                [1, 0, 0, v.x],
-                [0, 1, 0, v.y],
-                [0, 0, 1, v.z],
-                [0, 0, 0, 1],
-            ]
-        )
-        return np.multiply(translate, obj)
+        if v._is_3d == True:
+            translate = np.array(
+                [
+                    [1, 0, 0, v.x],
+                    [0, 1, 0, v.y],
+                    [0, 0, 1, v.z],
+                    [0, 0, 0, 1],
+                ]
+            )
+        else:
+            translate = np.array(
+                [
+                    [1, 0, v.x],
+                    [0, 1, v.y],
+                    [0, 0, 1],
+                ]
+            )
+        return np.matmul(translate, obj)
 
 
 class Scale(np.ndarray):
-    def __new__(cls, input: object, v: Vector3D):
+    def __new__(cls, input: object, v: Vector):
         obj = np.asarray(input).view(cls)
         scalar_matrix = np.array(
             [
--- tests/test_transformation.py
+++ tests/test_transformation.py
@@ -1,21 +1,24 @@
 import numpy as np
 
-from ansys.geometry.core.math import Matrix33, Vector3D
+from ansys.geometry.core.math import Matrix33, Vector
 from ansys.geometry.core.transformation import Rotation, Translation
 
-DOUBLE_EPS = np.finfo(float).eps
+DOUBLE_EPS = np.finfo(float).eps * 2
 
 
 def test_transformation_rotation():
-    v_1 = Vector3D([1, 2, 3])
+    v_1 = Vector([1, 2, 3])
     rot_x = Rotation(v_1, np.pi / 2, "x")
-    assert abs(all(rot_x - Vector3D([1, -1, 2]))) <= DOUBLE_EPS
+    test_vector = Vector([1, -3, 2])
+    assert rot_x.x - test_vector.x <= DOUBLE_EPS
+    assert rot_x.y - test_vector.y <= DOUBLE_EPS
+    assert rot_x.z - test_vector.z <= DOUBLE_EPS
     m = Matrix33()
     rot_x = Rotation(m, [np.pi / 2, np.pi / 2], "xy")
     test = np.array([[0, 0, -1], [1.0, 0, 0], [0.0, -1, 0]])
 
 
 def test_translation():
-    v_1 = Vector3D([1, 2, 3])
-    v_2 = Vector3D([1, 1, 1])
+    v_1 = Vector([1, 2, 3])
+    v_2 = Vector([1, 1, 1])
     trans = Translation(v_1, v_2)
