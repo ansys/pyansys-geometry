dad5a7608bc22d854506d0b496c67ab5be4452c5
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2022-09-12 14:42:17 +0200

    Initialise test with round values

--- tests/test_transformation.py
+++ tests/test_transformation.py
@@ -4,7 +4,7 @@ from ansys.geometry.core.math import Matrix33, Point, Vector
 from ansys.geometry.core.misc.units import UNITS
 from ansys.geometry.core.transformation import Rotation, Scaling, Translation
 
-DOUBLE_EPS = np.finfo(float).eps * 2
+DOUBLE_EPS = np.finfo(float).eps
 
 
 def test_transformation_rotation():
@@ -12,15 +12,15 @@ def test_transformation_rotation():
     v_1 = Vector([1, 2, 3])
     rot_x = Rotation(v_1, np.pi / 2, "x")
     test_vector = Vector([1, -3, 2])
-    assert rot_x.x - test_vector.x <= DOUBLE_EPS
-    assert rot_x.y - test_vector.y <= DOUBLE_EPS
-    assert rot_x.z - test_vector.z <= DOUBLE_EPS
+    assert round(rot_x.x) - test_vector.x <= DOUBLE_EPS
+    assert round(rot_x.y) - test_vector.y <= DOUBLE_EPS
+    assert round(rot_x.z) - test_vector.z <= DOUBLE_EPS
 
     # Rotation along x axis in degree
     rot_x_degree = Rotation(v_1, 90, "x", unit=UNITS.degree)
-    assert rot_x_degree.x - test_vector.x <= DOUBLE_EPS
-    assert rot_x_degree.y - test_vector.y <= DOUBLE_EPS
-    assert rot_x_degree.z - test_vector.z <= DOUBLE_EPS
+    assert round(rot_x_degree.x) - test_vector.x <= DOUBLE_EPS
+    assert round(rot_x_degree.y) - test_vector.y <= DOUBLE_EPS
+    assert round(rot_x_degree.z) - test_vector.z <= DOUBLE_EPS
 
     # Test matrix in x = 90, y = 90
     m = Matrix33()
@@ -37,7 +37,7 @@ def test_transformation_rotation():
 
 
 def test_transformation_translation():
-    """Simple test to test translation"""
+    """Simple test for translation"""
     point = Point([1, 2, 3])
     v = Vector([1, 1, 1])
     trans = Translation(point, v)
@@ -45,7 +45,7 @@ def test_transformation_translation():
 
 
 def test_transformation_scalar():
-    """Simple test to test scaling"""
+    """Simple test for scaling"""
     point = Point([1, 2, 3])
     v = Vector([1, 1 / 2, 1 / 3])
     scale = Scaling(point, v)
