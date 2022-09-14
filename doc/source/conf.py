3f64dbf533e8f3716ed9f1993d7b30a3824c218d
Jorge Martínez Garrido <jorge.martinezgarrido@ansys.com>
2022-09-15 00:09:50 +0200

    DOC: add intersphinx for pyvista and scipy

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -43,11 +43,11 @@ intersphinx_mapping = {
     "python": ("https://docs.python.org/dev", None),
     "pint": ("https://pint.readthedocs.io/en/stable", None),
     "numpy": ("https://numpy.org/devdocs", None),
+    "scipy": ("https://docs.scipy.org/doc/scipy/reference", None),
+    "pyvista": ("https://docs.pyvista.org/", None),
     # kept here as an example
-    # "scipy": ("https://docs.scipy.org/doc/scipy/reference", None),
     # "matplotlib": ("https://matplotlib.org/stable", None),
     # "pandas": ("https://pandas.pydata.org/pandas-docs/stable", None),
-    # "pyvista": ("https://docs.pyvista.org/", None),
     # "grpc": ("https://grpc.github.io/grpc/python/", None),
 }
 
