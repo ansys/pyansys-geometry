23334ffd2e79bc6287d6d934b736aafde4009c71
Alex Kaszynski <akascap@gmail.com>
2022-09-14 01:36:36 +0000

    use venv

--- src/ansys/geometry/core/connection/client.py
+++ src/ansys/geometry/core/connection/client.py
@@ -78,7 +78,6 @@ class GrpcClient:
             self._target = str(channel)
         else:
             self._target = f"{host}:{port}"
-            1 / 0
             self._channel = grpc.insecure_channel(
                 self._target,
                 options=[
