f7d543ad7cc83737aad8cf3d57e4feebd60f12c0
Roberto Pastor Muela <roberto.pastormuela@ansys.com>
2022-09-27 16:31:26 +0200

    Update src/ansys/geometry/core/shapes/slot.py
    Co-authored-by: Revathy Venugopal <104772255+Revathyvenugopal162@users.noreply.github.com>

--- src/ansys/geometry/core/shapes/slot.py
+++ src/ansys/geometry/core/shapes/slot.py
@@ -148,7 +148,7 @@ class Slot(BaseShape):
         Quantity
             The perimeter of the slot.
         """
-        return 2 * np.pi * (self._height.value / 2) + 2 * (self._width.value - self._height.value)
+        return np.pi * self._height.value + 2 * (self._width.value - self._height.value)
 
     @property
     def area(self) -> Quantity:
