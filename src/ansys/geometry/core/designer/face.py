f03e3e9eadaaff85d16d6e28aa4bfb76f48092ba
Chad Queen <chadqueen82@gmail.com>
2022-09-28 15:28:25 -0400

    Add functionality to create solid and surface bodies from an existing face

--- src/ansys/geometry/core/designer/face.py
+++ src/ansys/geometry/core/designer/face.py
@@ -3,6 +3,7 @@
 from enum import Enum, unique
 from typing import TYPE_CHECKING, List
 
+from ansys.api.geometry.v0.bodies_pb2_grpc import BodiesStub
 from ansys.api.geometry.v0.edges_pb2 import EdgeIdentifier
 from ansys.api.geometry.v0.edges_pb2_grpc import EdgesStub
 from ansys.api.geometry.v0.faces_pb2 import (
@@ -17,8 +18,7 @@ from pint import Quantity
 from ansys.geometry.core.connection import GrpcClient
 from ansys.geometry.core.designer.edge import CurveType, Edge
 from ansys.geometry.core.math import Point, UnitVector
-from ansys.geometry.core.misc import SERVER_UNIT_AREA
-from ansys.geometry.core.misc.measurements import SERVER_UNIT_LENGTH
+from ansys.geometry.core.misc import SERVER_UNIT_AREA, SERVER_UNIT_LENGTH
 
 if TYPE_CHECKING:
     from ansys.geometry.core.designer.body import Body  # pragma: no cover
@@ -136,11 +136,17 @@ class Face:
         self._grpc_client = grpc_client
         self._faces_stub = FacesStub(grpc_client.channel)
         self._edges_stub = EdgesStub(grpc_client.channel)
+        self._bodies_stub = BodiesStub(self._grpc_client.channel)
 
     @property
     def id(self) -> str:
         """ID of the face."""
         return self._id
+
+    @property
+    def body(self) -> "Body":
+        """The body to which the face belongs."""
+        return self._body
 
     @property
     def area(self) -> Quantity:
