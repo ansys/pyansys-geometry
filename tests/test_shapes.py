96fc63f7bf1820862649f70406349d57b5fa916b
Roberto Pastor Muela <roberto.pastormuela@ansys.com>
2022-10-04 17:42:33 +0200

    Revert "Solving arc test issues"
    This reverts commit 0abdfdc42f3a1f9ba7b9ded8148f81b5416f4838.

--- tests/test_shapes.py
+++ tests/test_shapes.py
@@ -476,10 +476,8 @@ def test_create_arc():
 
     # Check attributes are expected ones
     assert arc_clockwise.radius == 2 * UNITS.meter
-    assert arc_clockwise.sector_area == Quantity(
-        np.pi * (2 * 2) * (arc_clockwise.angle.m / (2 * np.pi)), UNITS.meter * UNITS.meter
-    )
-    assert arc_clockwise.length == (arc_clockwise.radius.m * arc_clockwise.angle.m) * UNITS.meter
+    assert arc_clockwise.sector_area == Quantity(np.pi, UNITS.meter * UNITS.meter)
+    assert arc_clockwise.length == np.pi * UNITS.meter
     assert (
         arc_counterclockwise.length
         == 2 * np.pi * arc_counterclockwise.radius - arc_clockwise.length
