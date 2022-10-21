020c54e7a1adbdf5962ef63bf0e3d46f78b62456
Revathy Venugopal <104772255+Revathyvenugopal162@users.noreply.github.com>
2022-10-21 10:26:59 +0200

    Add basic design examples and arrange the format of example page. (#183)
    Co-authored-by: Roberto Pastor Muela <roberto.pastormuela@ansys.com>
    Co-authored-by: Maxime Rey <87315832+MaxJPRey@users.noreply.github.com>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -120,9 +120,12 @@ nbsphinx_custom_formats = {
     ".mystnb": ["jupytext.reads", {"fmt": "mystnb"}],
 }
 nbsphinx_thumbnails = {
-    "examples/basic_usage": "_static/thumbnails/basic_usage.png",
-    "examples/dynamic_sketch_plane": "_static/thumbnails/dynamic_sketch_plane.png",
-    "examples/design_organization": "_static/thumbnails/design_organization.png",
+    "examples/basic/basic_usage": "_static/thumbnails/basic_usage.png",
+    "examples/design/dynamic_sketch_plane": "_static/thumbnails/dynamic_sketch_plane.png",
+    "examples/design/add_design_material": "_static/thumbnails/add_design_material.png",
+    "examples/design/plate_with_hole": "_static/thumbnails/plate_with_hole.png",
+    "examples/design/tessellation_usage": "_static/thumbnails/tessellation_usage.png",
+    "examples/design/design_organization": "_static/thumbnails/design_organization.png",
 }
 
 typehints_defaults = "comma"
