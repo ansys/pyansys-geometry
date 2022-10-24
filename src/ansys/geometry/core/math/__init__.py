9394d4bc66998f7496f7c075d963dd667368f757
Roberto Pastor Muela <roberto.pastormuela@ansys.com>
2022-10-24 11:38:37 +0200

    Removing ``__all__`` statements

--- src/ansys/geometry/core/math/__init__.py
+++ src/ansys/geometry/core/math/__init__.py
@@ -1,4 +1,5 @@
 """PyGeometry math subpackage."""
+
 from ansys.geometry.core.math.bbox import BoundingBox2D
 from ansys.geometry.core.math.constants import (
     DEFAULT_POINT2D,
@@ -18,30 +19,3 @@ from ansys.geometry.core.math.matrix import Matrix, Matrix33, Matrix44
 from ansys.geometry.core.math.plane import Plane
 from ansys.geometry.core.math.point import Point2D, Point3D
 from ansys.geometry.core.math.vector import UnitVector2D, UnitVector3D, Vector2D, Vector3D
-
-__all__ = [
-    "DEFAULT_POINT",
-    "DEFAULT_POINT2D",
-    "DEFAULT_POINT3D",
-    "UNITVECTOR2D_X",
-    "UNITVECTOR2D_Y",
-    "UNITVECTOR3D_X",
-    "UNITVECTOR3D_Y",
-    "UNITVECTOR3D_Z",
-    "ZERO_POINT2D",
-    "ZERO_POINT3D",
-    "ZERO_VECTOR2D",
-    "ZERO_VECTOR3D",
-    "BoundingBox2D",
-    "Frame",
-    "Matrix",
-    "Matrix33",
-    "Matrix44",
-    "Plane",
-    "Point2D",
-    "Point3D",
-    "UnitVector2D",
-    "UnitVector3D",
-    "Vector2D",
-    "Vector3D",
-]
