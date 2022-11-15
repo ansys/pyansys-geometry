3f5198c81c8da4d1a6da018cfe254bb7d9244716
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2022-11-15 17:40:36 +0100

    Add test for center of the body got tesselated

--- tests/integration/test_tessellation.py
+++ tests/integration/test_tessellation.py
@@ -1,21 +1,25 @@
 """Test tessellation and plotting."""
 from ansys.geometry.core import Modeler
-from ansys.geometry.core.math import Plane, Point2D, Point3D
+from ansys.geometry.core.math import Plane, Point2D
 from ansys.geometry.core.misc.units import UNITS
 from ansys.geometry.core.sketch import Sketch
 
 
 def test_body_tessellate(modeler: Modeler):
-    origin = Point3D([0, 0, 0])
-    plane = Plane(origin, direction_x=[1, 0, 0], direction_y=[0, 0, 1])
-    sketch = Sketch(plane)
-    box = sketch.box(Point2D([2, 0]), 4, 4)
-    design = modeler.create_design("my-design")
-    my_comp = design.add_component("my-comp")
-    body = my_comp.extrude_sketch("my-sketch", sketch, 1 * UNITS.m)
+    """Test the body tessellation"""
+    sketch = Sketch()
+    sketch.box(Point2D([2, 0]), 4, 4)
+    design = modeler.create_design("Design1")
+    my_comp = design.add_component("Component1")
+    body = my_comp.extrude_sketch("Sketch1", sketch, 1 * UNITS.m)
+
+    # Tessellate the body without merging the individual faces
     blocks = body.tessellate()
     assert "MultiBlock" in str(blocks)
     assert blocks.n_blocks == 6
+    assert (blocks.center == ([2, 0, 0.5])).all()
+
+    # Tessellate the body without merging the individual faces
     mesh = body.tessellate(merge=True)
     assert "PolyData" in str(mesh)
     assert mesh.n_cells == 12
@@ -24,16 +28,17 @@ def test_body_tessellate(modeler: Modeler):
 
 
 def test_component_tessellate(modeler: Modeler):
+    """Test the component tessellation"""
     sketch_1 = Sketch()
     sketch_1.box(Point2D([10, 10]), width=10, height=5)
     sketch_1.circle(Point2D([0, 0]), radius=25 * UNITS.m)
-    design = modeler.create_design("MyDesign")
-    comp = design.add_component("MyComponent")
-    comp.extrude_sketch("MyBody", sketch=sketch_1, distance=10 * UNITS.m)
+    design = modeler.create_design("Design")
+    comp = design.add_component("Component")
+    comp.extrude_sketch("Body", sketch=sketch_1, distance=10 * UNITS.m)
     sketch_2 = Sketch(Plane([0, 0, 10]))
     sketch_2.box(Point2D([10, 10]), width=10, height=5)
     sketch_2.circle(Point2D([0, 0]), radius=25 * UNITS.m)
-    comp.extrude_sketch("MyBody", sketch=sketch_2, distance=10 * UNITS.m)
+    comp.extrude_sketch("Body", sketch=sketch_2, distance=10 * UNITS.m)
     dataset = comp.tessellate(merge_bodies=True)
     assert "MultiBlock" in str(dataset)
     assert dataset.n_blocks == 1
