f2f769acd9be454a4db238b1eb0d1915fd4c9f60
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2022-11-16 12:48:25 +0100

    Check bounds with teseelation

--- tests/integration/test_tessellation.py
+++ tests/integration/test_tessellation.py
@@ -1,4 +1,6 @@
 """Test tessellation and plotting."""
+import pytest
+
 from ansys.geometry.core import Modeler
 from ansys.geometry.core.math import Plane, Point2D
 from ansys.geometry.core.misc.units import UNITS, Quantity
@@ -18,7 +20,11 @@ def test_body_tessellate(modeler: Modeler):
     assert "MultiBlock" in str(blocks_1)
     # Number of blocks will be number of faces
     assert blocks_1.n_blocks == 6
+    # test the center of bounding box
     assert (blocks_1.center == ([2, 0, 0.5])).all()
+    # Test the values of blocks which are the length 6 tuple of floats
+    # containing min/max along each axis
+    assert blocks_1.bounds == [0.0, 4.0, -2.0, 2.0, 0.0, 1.0]
 
     # Tessellate the body without merging the individual faces
     mesh_1 = body_1.tessellate(merge=True)
@@ -26,6 +32,7 @@ def test_body_tessellate(modeler: Modeler):
     # Test number of cells, points and arrays in dataset
     assert mesh_1.n_cells == 12
     assert mesh_1.n_points == 24
+    assert blocks_1.bounds == [0.0, 4.0, -2.0, 2.0, 0.0, 1.0]
     assert mesh_1.n_arrays == 0
 
     sketch_2 = Sketch()
@@ -38,6 +45,11 @@ def test_body_tessellate(modeler: Modeler):
     blocks_2 = body_2.tessellate()
     assert "MultiBlock" in str(blocks_2)
     assert blocks_2.n_blocks == 3
+    assert blocks_2.bounds == pytest.approx(
+        [0.019999999999999997, 0.04, 0.020151922469877917, 0.03984807753012208, 0.0, 0.03],
+        rel=1e-6,
+        abs=1e-8,
+    )
     assert (blocks_2.center == ([0.03, 0.03, 0.015])).all()
 
     # Tessellate the body without merging the individual faces
@@ -50,13 +62,19 @@ def test_body_tessellate(modeler: Modeler):
 
 def test_component_tessellate(modeler: Modeler):
     """Test the component tessellation"""
+
+    # Create a sketch
     sketch_1 = Sketch()
     sketch_1.box(Point2D([10, 10], UNITS.m), Quantity(10, UNITS.m), Quantity(5, UNITS.m))
     sketch_1.circle(Point2D([0, 0], UNITS.m), Quantity(25, UNITS.m))
+
+    # Create the design on the server side
     design = modeler.create_design("Design")
     comp = design.add_component("Component")
     distance = Quantity(10, UNITS.m)
     comp.extrude_sketch("Body", sketch=sketch_1, distance=distance)
+
+    # Create another sketch in different plane
     sketch_2 = Sketch(Plane([0, 0, 10]))
     sketch_2.box(Point2D([10, 10], UNITS.m), Quantity(10, UNITS.m), Quantity(5, UNITS.m))
     sketch_2.circle(Point2D([0, 0], UNITS.m), Quantity(25, UNITS.m))
@@ -68,7 +86,11 @@ def test_component_tessellate(modeler: Modeler):
     assert "MultiBlock" in str(dataset)
     assert dataset.n_blocks == 1
     assert (dataset.center == ([0.0, 0.0, 10.0])).all()
-
+    assert dataset.bounds == pytest.approx(
+        [-25.0, 25.0, -24.999251562526105, 24.999251562526105, 0.0, 20.0],
+        rel=1e-6,
+        abs=1e-8,
+    )
     # Tessellate the component by merging it to single dataset.
     mesh = comp.tessellate(merge_component=True)
     assert "PolyData" in str(mesh)
@@ -76,3 +98,8 @@ def test_component_tessellate(modeler: Modeler):
     assert mesh.n_faces == 3280
     assert mesh.n_arrays == 0
     assert mesh.n_points == 3300
+    assert dataset.bounds == pytest.approx(
+        [-25.0, 25.0, -24.999251562526105, 24.999251562526105, 0.0, 20.0],
+        rel=1e-6,
+        abs=1e-8,
+    )
