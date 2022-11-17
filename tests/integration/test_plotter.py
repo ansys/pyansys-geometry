8aae6965643cee04ebd6a1744e1a81329ce747c1
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2022-11-17 12:29:50 +0100

    Add teset for `visualization_polydata`

--- tests/integration/test_plotter.py
+++ tests/integration/test_plotter.py
@@ -7,7 +7,18 @@ from ansys.geometry.core import Modeler
 from ansys.geometry.core.math import Point2D
 from ansys.geometry.core.misc import UNIT_LENGTH, UNITS, Distance
 from ansys.geometry.core.plotting import Plotter
-from ansys.geometry.core.sketch import Sketch
+from ansys.geometry.core.sketch import (
+    Arc,
+    Box,
+    Circle,
+    Ellipse,
+    Polygon,
+    Segment,
+    Sketch,
+    Slot,
+    Trapezoid,
+    Triangle,
+)
 
 skip_no_xserver = pytest.mark.skipif(
     not system_supports_plotting(), reason="Requires active X Server"
@@ -15,7 +26,26 @@ skip_no_xserver = pytest.mark.skipif(
 
 
 @skip_no_xserver
+def test_body_plot(modeler: Modeler):
+    """Test plotting the body."""
+
+    # Create a Sketch
+    sketch = Sketch()
+    sketch.box(Point2D([10, 10], UNITS.mm), Quantity(10, UNITS.mm), Quantity(10, UNITS.mm))
+
+    # Create your design on the server side
+    design_name = "BoxExtrusions"
+    design = modeler.create_design(design_name)
+
+    # Extrude the sketch to create a Body
+    box_body = design.extrude_sketch("JustABox", sketch, Quantity(10, UNITS.mm))
+    box_body.plot()
+
+
+@skip_no_xserver
 def test_component_plot(modeler: Modeler):
+    """Test plotting the component."""
+
     # Create a Sketch
     sketch = Sketch()
     sketch.box(Point2D([10, 10], UNITS.mm), Quantity(10, UNITS.mm), Quantity(10, UNITS.mm))
@@ -33,21 +63,6 @@ def test_component_plot(modeler: Modeler):
 
     # Test the plotting of the component
     design.plot()
-
-
-@skip_no_xserver
-def test_body_plot(modeler: Modeler):
-    # Create a Sketch
-    sketch = Sketch()
-    sketch.box(Point2D([10, 10], UNITS.mm), Quantity(10, UNITS.mm), Quantity(10, UNITS.mm))
-
-    # Create your design on the server side
-    design_name = "BoxExtrusions"
-    design = modeler.create_design(design_name)
-
-    # Extrude the sketch to create a Body
-    box_body = design.extrude_sketch("JustABox", sketch, Quantity(10, UNITS.mm))
-    box_body.plot()
 
 
 @skip_no_xserver
@@ -88,7 +103,7 @@ def test_plot_sketches():
 
     # Create a ellipse and plot
     sketch.ellipse(
-        Point2D([0, 0]), UNITS.m, Quantity(2, UNITS.m), Quantity(1, UNITS.m), tag="Ellipse"
+        Point2D([0, 0], UNITS.m), Quantity(2, UNITS.m), Quantity(1, UNITS.m), tag="Ellipse"
     )
     sketch.plot_selection()
     sketch.select("Ellipse")
@@ -119,9 +134,87 @@ def test_plot_sketches():
 
 @skip_no_xserver
 def test_plot_sketch_scene():
+    """Test plotting a sketch in the scene."""
+
+    # Create a sketch
     sketch = Sketch()
     sketch.polygon(Point2D([10, 10], UNITS.mm), Quantity(10, UNITS.mm), sides=5)
-    sketch.segment(Point2D([3, 2]), Point2D([2, 0]), "Segment4")
+    sketch.segment(Point2D([3, 2]), Point2D([2, 0]), "Segment")
+
+    # initialize the ``Plotter`` class
     pl = Plotter()
+
+    # showing the plane of the sketch and its frame.
     pl.plot_sketch(sketch=sketch, show_frame=True, show_plane=True)
     pl.scene.show()
+
+
+def test_visualization_polydata():
+    slot = Slot(
+        Point2D([2, 3], unit=UNITS.meter),
+        Distance(4, unit=UNITS.meter),
+        Distance(2, unit=UNITS.meter),
+    )
+    assert slot.visualization_polydata
+    assert slot.visualization_polydata.bounds == pytest.approx(
+        [0.0, 4.0, 2.0, 4.0, 0.0, 0.0],
+        rel=1e-6,
+        abs=1e-8,
+    )
+    # assert (slot.visualization_polydata.center == [2.0, 3.0, 0.0]).all()
+    assert slot.visualization_polydata.n_faces == 6
+    assert slot.visualization_polydata.n_cells == 6
+    assert slot.visualization_polydata.n_points == 402
+    assert slot.visualization_polydata.n_open_edges == 0
+
+    segment = Segment(Point2D([3, 2]), Point2D([2, 0]))
+    assert segment.visualization_polydata.n_faces == 1
+    assert segment.visualization_polydata.n_cells == 1
+    assert segment.visualization_polydata.n_points == 2
+    assert segment.visualization_polydata.n_open_edges == 0
+
+    polygon = Polygon(Point2D([10, 10], UNITS.mm), Quantity(10, UNITS.mm), sides=5)
+    assert polygon.visualization_polydata.n_faces == 2
+    assert polygon.visualization_polydata.n_cells == 2
+    assert polygon.visualization_polydata.n_points == 5
+    assert polygon.visualization_polydata.n_open_edges == 5
+
+    arc = Arc(Point2D([10, 0]), Point2D([10, 10]), Point2D([10, -10]))
+    assert arc.visualization_polydata.n_faces == 2
+    assert arc.visualization_polydata.n_cells == 2
+    assert arc.visualization_polydata.n_points == 202
+    assert arc.visualization_polydata.n_open_edges == 0
+
+    triangle = Triangle(Point2D([10, 10]), Point2D([2, 1]), Point2D([10, -10]))
+    assert triangle.visualization_polydata.n_faces == 1
+    assert triangle.visualization_polydata.n_cells == 1
+    assert triangle.visualization_polydata.n_points == 3
+    assert triangle.visualization_polydata.n_open_edges == 3
+
+    trapezoid = Trapezoid(10, 8, np.pi / 4, np.pi / 8, Point2D([10, -10]))
+    assert trapezoid.visualization_polydata.n_faces == 1
+    assert trapezoid.visualization_polydata.n_cells == 1
+    assert trapezoid.visualization_polydata.n_points == 4
+    assert trapezoid.visualization_polydata.n_open_edges == 4
+
+    circle = Circle(Point2D([10, -10], UNIT_LENGTH), Quantity(1, UNIT_LENGTH))
+    assert circle.visualization_polydata.n_faces == 1
+    assert circle.visualization_polydata.n_cells == 1
+    assert circle.visualization_polydata.n_points == 100
+    assert circle.visualization_polydata.n_open_edges == 100
+
+    ellipse = Ellipse(Point2D([0, 0], UNITS.m), Quantity(1, UNITS.m), Quantity(1, UNITS.m))
+    assert ellipse.visualization_polydata.n_faces == 1
+    assert ellipse.visualization_polydata.n_cells == 1
+    assert ellipse.visualization_polydata.n_points == 100
+    assert ellipse.visualization_polydata.n_open_edges == 100
+
+    box = Box(
+        Point2D([3, 1], unit=UNITS.meter),
+        Distance(4, unit=UNITS.meter),
+        Distance(2, unit=UNITS.meter),
+    )
+    assert box.visualization_polydata.n_faces == 1
+    assert box.visualization_polydata.n_cells == 1
+    assert box.visualization_polydata.n_points == 4
+    assert box.visualization_polydata.n_open_edges == 4
