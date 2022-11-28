8f09a4b31dc3e5e035f2d6535aab33ec798579b7
Roberto Pastor Muela <roberto.pastormuela@ansys.com>
2022-11-28 17:37:02 +0100

    Adding PlotterWidget abstract class

---
+++ src/ansys/geometry/core/plotting/widgets/widget.py
@@ -0,0 +1,27 @@
+"""Module containing the abstract implementation of Plotter widgets."""
+
+from abc import ABC, abstractmethod
+
+from pyvista import Plotter
+
+
+class PlotterWidget(ABC):
+    """Abstract class for Plotter widgets.
+
+    Notes
+    -----
+    These widgets are intended to be used with PyVista plotter objects.
+    """
+
+    def __init__(self, plotter: Plotter):
+        self._plotter: Plotter = plotter
+
+    @abstractmethod
+    def callback(self) -> None:
+        """General callback function for PlotterWidget objects."""
+        pass
+
+    @abstractmethod
+    def update(self) -> None:
+        """General update function for PlotterWidget objects."""
+        pass
