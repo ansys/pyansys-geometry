edfe0519c8db0c27dbb646e9c1e19b05310c36d8
jonahrb <56607167+jonahrb@users.noreply.github.com>
2023-01-05 11:50:35 -0500

    Implementation of Circle and Line primitive clases + evaluation (#302)
    Co-authored-by: Roberto Pastor Muela <roberto.pastormuela@ansys.com>

---
+++ src/ansys/geometry/core/primitives/curve_evaluation.py
@@ -0,0 +1,34 @@
+from ansys.geometry.core.math.point import Point3D
+from ansys.geometry.core.math.vector import Vector3D
+
+
+class CurveEvaluation:
+    """Provides result class when evaluating a curve"""
+
+    def __init__(self, parameter: float = None) -> None:
+        self._parameter = parameter
+
+    def is_set(self) -> bool:
+        """Returns whether or not the parameter has been set"""
+        return self._parameter is not None
+
+    @property
+    def parameter(self) -> float:
+        """The parameter that the evaluation is based upon"""
+        raise NotImplementedError("Each evaluation must provide the parameter definition.")
+
+    def position(self) -> Point3D:
+        """The position of the evaluation"""
+        raise NotImplementedError("Each evaluation must provide the position definition.")
+
+    def first_derivative(self) -> Vector3D:
+        """The first derivative of the evaluation"""
+        raise NotImplementedError("Each evaluation must provide the first_derivative definition.")
+
+    def second_derivative(self) -> Vector3D:
+        """The second derivative of the evaluation"""
+        raise NotImplementedError("Each evaluation must provide the second_derivative definition.")
+
+    def curvature(self) -> float:
+        """The curvature of the evaluation"""
+        raise NotImplementedError("Each evaluation must provide the curvature definition.")
