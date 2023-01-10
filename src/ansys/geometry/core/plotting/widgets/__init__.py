f54655e697be475647ccece08d3aa315d92bcdcd
Roberto Pastor Muela <roberto.pastormuela@ansys.com>
2023-01-10 11:02:11 +0100

    Missing imports in subpackage

--- src/ansys/geometry/core/plotting/widgets/__init__.py
+++ src/ansys/geometry/core/plotting/widgets/__init__.py
@@ -1,2 +1,10 @@
+from ansys.geometry.core.plotting.widgets.displace_arrows import (
+    DisplaceArrowXDown,
+    DisplaceArrowXUp,
+    DisplaceArrowYDown,
+    DisplaceArrowYUp,
+    DisplaceArrowZDown,
+    DisplaceArrowZUp,
+)
 from ansys.geometry.core.plotting.widgets.ruler import Ruler
 from ansys.geometry.core.plotting.widgets.widget import PlotterWidget
