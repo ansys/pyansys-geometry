f202316f9dd6e56e8e36980309914f29a99c5fed
jonahrb <56607167+jonahrb@users.noreply.github.com>
2023-01-16 09:51:01 -0500

    Update src/ansys/geometry/core/primitives/__init__.py
    Co-authored-by: Roberto Pastor Muela <roberto.pastormuela@ansys.com>

--- src/ansys/geometry/core/primitives/__init__.py
+++ src/ansys/geometry/core/primitives/__init__.py
@@ -1,6 +1,6 @@
 """Provides the PyGeometry ``primitives`` subpackage."""
 
-from ansys.geometry.core.primitives.circle import Circle
+from ansys.geometry.core.primitives.circle import Circle, CircleEvaluation
 from ansys.geometry.core.primitives.cone import Cone
 from ansys.geometry.core.primitives.cylinder import Cylinder
 from ansys.geometry.core.primitives.ellipse import Ellipse, EllipseEvaluation
