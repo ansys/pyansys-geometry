9c5f68e2be4c3a1805a6812db9160a972cd178f1
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2023-02-22 09:51:30 +0100

    Update src/ansys/geometry/core/primitives/cylinder.py

--- src/ansys/geometry/core/primitives/cylinder.py
+++ src/ansys/geometry/core/primitives/cylinder.py
@@ -113,7 +113,7 @@ class Cylinder:
 
         Parameters
         ----------
-        height : Quantity, Distance, Real
+        height : Union[Quantity, Distance, Real]
             By nature, a cylinder is infinite. If you want to get the volume,
             you must bound it by a height. Normally a cylinder surface is not closed
             (does not have "caps" on the ends). This method will assume it is closed
