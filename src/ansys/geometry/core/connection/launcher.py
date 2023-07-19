1f3fd81539e9bb0917d267ec539c6faa0c862d4c
Matteo Bini <91963243+b-matteo@users.noreply.github.com>
2023-07-19 22:14:14 +0200

    Update src/ansys/geometry/core/connection/launcher.py
    Co-authored-by: Jonah Boling <56607167+jonahrb@users.noreply.github.com>

--- src/ansys/geometry/core/connection/launcher.py
+++ src/ansys/geometry/core/connection/launcher.py
@@ -196,7 +196,7 @@ def launch_discovery(version: Optional[str] = "241", **kwargs: Optional[Dict]) -
 
     Parameters
     ----------
-    version : str, default: None
+    version : str, default: "241"
         Version of Discovery to run in the three-digit format.
         For example, "212". If you do not specify the version, the server
         chooses the version.
