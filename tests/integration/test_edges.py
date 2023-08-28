cbf0380fbc01be27763791b0ad3fb2106ddf3eb1
afernand <alex.fernandezluces@ansys.com>
2023-08-28 15:18:43 +0200

    Add test to check if start and end point of edges are fine

---
+++ tests/integration/test_edges.py
@@ -0,0 +1,23 @@
+"""Test edges."""
+
+from ansys.geometry.core import Modeler
+from ansys.geometry.core.math import Point2D, Vector3D
+from ansys.geometry.core.misc.units import UNITS, Quantity
+from ansys.geometry.core.sketch import Sketch
+
+
+def test_edges_startend(modeler: Modeler):
+    # init modeler
+    design = modeler.create_design("Multiplot")
+
+    # define sketch
+    sketch = Sketch()
+    sketch.polygon(Point2D([-10, 10], UNITS.m), Quantity(10, UNITS.m), sides=5, tag="Polygon1")
+
+    # define body
+    body_sketch = Sketch()
+    body_sketch.box(Point2D([10, 10], UNITS.m), Quantity(10, UNITS.m), Quantity(10, UNITS.m))
+    box_body = design.extrude_sketch("JustABox", body_sketch, Quantity(10, UNITS.m))
+    for edge in box_body.edges:
+        vec = Vector3D.from_points(edge.start_point, edge.end_point)
+        assert vec.magnitude == 10
