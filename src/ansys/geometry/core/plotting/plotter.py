d2e5a0d7a714c330e16e5c7c0aded0b2f9f53a3b
afernand <alex.fernandezluces@ansys.com>
2023-08-30 13:00:26 +0200

    Remove unneeded color assignment

--- src/ansys/geometry/core/plotting/plotter.py
+++ src/ansys/geometry/core/plotting/plotter.py
@@ -282,7 +282,6 @@ class Plotter:
             actor, _ = self.scene.add_composite(dataset, **plotting_options)
         else:
             actor = self.scene.add_mesh(dataset, **plotting_options)
-        actor.prop.color = DEFAULT_COLOR
         return actor.name
 
     def add_sketch_polydata(self, polydata_entries: List[pv.PolyData], **plotting_options) -> None:
