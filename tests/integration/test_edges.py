2ee3b09d31ee55f765bde0661f3c6319d77179d5
afernand <alex.fernandezluces@ansys.com>
2023-08-30 16:09:44 +0200

    Add cylinder test

--- tests/integration/test_edges.py
+++ tests/integration/test_edges.py
@@ -17,3 +17,15 @@ def test_edges_startend_prism(modeler: Modeler):
     for edge in prism_body.edges:
         vec = Vector3D.from_points(edge.start_point, edge.end_point)
         assert vec.magnitude == edge.length.magnitude
+
+
+def test_edges_startend_cylinder(modeler: Modeler):
+    # init modeler
+    design = modeler.create_design("cylinder")
+
+    # define body
+    cylinder = Sketch()
+    cylinder.circle(Point2D([10, 10], UNITS.m), 1.0)
+    cylinder_body = design.extrude_sketch("JustACyl", cylinder, Quantity(10, UNITS.m))
+    for edge in cylinder_body.edges:
+        assert edge.start_point == edge.end_point
