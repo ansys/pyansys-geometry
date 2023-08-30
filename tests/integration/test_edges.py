addbbff56a98ca101437ec54550d580ca5cbb1e0
afernand <alex.fernandezluces@ansys.com>
2023-08-30 15:26:25 +0200

    Change box for a prism

--- tests/integration/test_edges.py
+++ tests/integration/test_edges.py
@@ -6,14 +6,14 @@ from ansys.geometry.core.misc.units import UNITS, Quantity
 from ansys.geometry.core.sketch import Sketch
 
 
-def test_edges_startend_box(modeler: Modeler):
+def test_edges_startend_prism(modeler: Modeler):
     # init modeler
-    design = modeler.create_design("Box")
+    design = modeler.create_design("Prism")
 
     # define body
     body_sketch = Sketch()
-    body_sketch.box(Point2D([10, 10], UNITS.m), Quantity(10, UNITS.m), Quantity(10, UNITS.m))
-    box_body = design.extrude_sketch("JustABox", body_sketch, Quantity(10, UNITS.m))
-    for edge in box_body.edges:
+    body_sketch.box(Point2D([10, 10], UNITS.m), Quantity(5, UNITS.m), Quantity(3, UNITS.m))
+    prism_body = design.extrude_sketch("JustAPrism", body_sketch, Quantity(13, UNITS.m))
+    for edge in prism_body.edges:
         vec = Vector3D.from_points(edge.start_point, edge.end_point)
         assert vec.magnitude == edge.length.magnitude
