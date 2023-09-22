58a9398105a9b93f823b472f12309797b1268ca3
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2023-09-22 16:37:10 +0200

    feat: allow warnings for rc

--- doc/make.bat
+++ doc/make.bat
@@ -8,7 +8,7 @@ if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
 if "%SPHINXOPTS%" == "" (
-	set SPHINXOPTS=-j auto -W --color
+	set SPHINXOPTS=-j auto
 )
 set SOURCEDIR=source
 set BUILDDIR=_build
