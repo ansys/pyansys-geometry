32b4968dcc3365947022c5c763e9ef3a108bfae6
Umut Soysal <umut.soysal@ansys.com>
2023-10-03 11:50:21 -0500

    Test files are converted to scdocx from scdoc to comply with parasolid kernel

--- tests/integration/test_repair_tools.py
+++ tests/integration/test_repair_tools.py
@@ -5,29 +5,28 @@ from ansys.geometry.core.tools import RepairTools
 
 
 def test_find_split_edges(modeler: Modeler):
-    modeler.open_file("./tests/integration/files/SplitEdgeDesignTest.scdoc")
+    modeler.open_file("./tests/integration/files/SplitEdgeDesignTest.scdocx")
     problem_areas = modeler.repair_tools.find_split_edges(["0:39"], 25, 150)
     assert len(problem_areas) == 3
 
 
 def test_find_extra_edges(modeler: Modeler):
     """Test to read geometry and find it's extra edge problem areas."""
-    modeler.open_file("./tests/integration/files/ExtraEdgesDesignBefore.scdoc")
+    modeler.open_file("./tests/integration/files/ExtraEdgesDesignBefore.scdocx")
     problem_areas = modeler.repair_tools.find_extra_edges(["0:22"])
     assert len(problem_areas) == 1
 
 
-def test_find_inexact_edges():
+def test_find_inexact_edges(modeler: Modeler):
     """Test to read geometry and find it's inexact edge problem areas."""
-    modeler = Modeler(host="localhost", port=50051)
-    modeler.open_file("./tests/integration/files/InExactEdgesBefore.scdoc")
+    modeler.open_file("./tests/integration/files/InExactEdgesBefore.scdocx")
     problem_areas = modeler.repair_tools.find_inexact_edges(["0:38"])
     assert len(problem_areas) == 6
 
 
 def test_find_missing_faces(modeler: Modeler):
     """Test to read geometry and find it's missing face problem areas."""
-    modeler.open_file("./tests/integration/files/MissingFacesDesignBefore.scdoc")
+    modeler.open_file("./tests/integration/files/MissingFacesDesignBefore.scdocx")
     repair_tools = RepairTools(modeler.client)
     problem_areas = repair_tools.find_missing_faces(["1:40"])
     assert len(problem_areas) == 1
@@ -35,21 +34,21 @@ def test_find_missing_faces(modeler: Modeler):
 
 def test_find_duplicate_faces(modeler: Modeler):
     """Test to read geometry and find it's duplicate face problem areas."""
-    modeler.open_file("./tests/integration/files/DuplicateFacesDesignBefore.scdoc")
+    modeler.open_file("./tests/integration/files/DuplicateFacesDesignBefore.scdocx")
     problem_areas = modeler.repair_tools.find_duplicate_faces(["0:22", "0:85"])
     assert len(problem_areas) == 1
 
 
 def test_find_small_faces(modeler: Modeler):
     """Test to read geometry and find it's small face problem areas."""
-    modeler.open_file("./tests/integration/files/SmallFacesBefore.scdoc")
+    modeler.open_file("./tests/integration/files/SmallFacesBefore.scdocx")
     problem_areas = modeler.repair_tools.find_small_faces(["0:38"])
     assert len(problem_areas) == 4
 
 
 def test_find_stitch_faces(modeler: Modeler):
     """Test to read geometry and find it's stitch face problem areas."""
-    modeler.open_file("./tests/integration/files/stitch_before.scdoc")
+    modeler.open_file("./tests/integration/files/stitch_before.scdocx")
     design = modeler.read_existing_design()
     faceIds = []
     for body in design.bodies:
