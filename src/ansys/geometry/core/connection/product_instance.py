e4a0034be1705f200c6af2b34b30aa650a0a997b
Matteo Bini <91963243+b-matteo@users.noreply.github.com>
2023-10-28 08:48:59 +0200

    Update src/ansys/geometry/core/connection/product_instance.py
    Co-authored-by: Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>

--- src/ansys/geometry/core/connection/product_instance.py
+++ src/ansys/geometry/core/connection/product_instance.py
@@ -279,7 +279,7 @@ def _manifest_path_provider(
     if manifest_path != None and os.path.exists(manifest_path):
         return manifest_path
     elif manifest_path != None and not os.path.exists(manifest_path):
-        print("specified manifest file's path does not exists. Taking install default path.")
+        LOG.info("Specified manifest file's path does not exists. Taking install default path.")
         return os.path.join(
             available_installations[version], ADDINS_SUBFOLDER, BACKEND_SUBFOLDER, MANIFEST_FILENAME
         )
