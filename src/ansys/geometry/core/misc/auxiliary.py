dfb97358f64a6a2836347c2178bbe61c07d044c7
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2024-02-07 10:50:52 +0100

    fix: pre-commit

--- src/ansys/geometry/core/misc/auxiliary.py
+++ src/ansys/geometry/core/misc/auxiliary.py
@@ -1,13 +1,35 @@
+# Copyright (C) 2023 - 2024 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 """Auxiliary functions for the PyAnsys Geometry library."""
 
 from beartype.typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from ansys.geometry.core.designer.component import Component
-    from ansys.geometry.core.designer.edge import Edge
     from ansys.geometry.core.designer.body import Body
-    from ansys.geometry.core.designer.face import Face
+    from ansys.geometry.core.designer.component import Component
     from ansys.geometry.core.designer.design import Design
+    from ansys.geometry.core.designer.edge import Edge
+    from ansys.geometry.core.designer.face import Face
 
 
 def get_design_from_component(component: "Component") -> "Design":
