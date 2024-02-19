1b5e97eb4187a1b5dfd20c812bd30728a287c5e3
Jorge Martinez <jorge.martinezgarrido@ansys.com>
2024-02-19 10:00:53 +0100

    fix: remove custom sphinx-autoapi branch

--- doc/make.bat
+++ doc/make.bat
@@ -13,13 +13,6 @@ if "%SPHINXOPTS%" == "" (
 set SOURCEDIR=source
 set BUILDDIR=_build
 
-REM TODO: these lines of code should be removed once the feature branch is merged
-for /f %%i in ('pip freeze ^| findstr /c:"sphinx-autoapi @ git+https://github.com/ansys/sphinx-autoapi"') do set is_custom_sphinx_autoapi_installed=%%i
-if NOT "%is_custom_sphinx_autoapi_installed%" == "sphinx-autoapi" (
-	pip uninstall --yes sphinx-autoapi
-	pip install "sphinx-autoapi @ git+https://github.com/ansys/sphinx-autoapi@feat/single-page-stable")
-REM TODO: these lines of code should be removed once the feature branch is merged
-
 if "%1" == "" goto help
 if "%1" == "clean" goto clean
 if "%1" == "pdf" goto pdf
