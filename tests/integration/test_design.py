f9cc0dae338a3b7cf0f90775c40e9a1337c74ce2
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2024-03-06 14:38:35 +0100

    fix: error in test

--- tests/integration/test_design.py
+++ tests/integration/test_design.py
@@ -209,8 +209,10 @@ def test_extrude_negative_sketch(modeler: Modeler):
     assert np.isclose(invalid_neg.faces[0].face_normal().dot(pos.faces[0].face_normal()), 1.0)
 
 
-def test_extrude_negative_sketch_face(modeler: Modeler):
+def test_extrude_negative_sketch_face():
     """Test to check the extrusion of a face in the negative direction."""
+    from ansys.geometry.core import launch_modeler_with_spaceclaim
+    modeler = launch_modeler_with_spaceclaim(hidden=True)
     # Create a sketch of a rectangle
     sk = Sketch()
     sk.box(Point2D([0, 0]), 10, 20)
@@ -228,7 +230,7 @@ def test_extrude_negative_sketch_face(modeler: Modeler):
     assert np.isclose(neg.faces[0].face_normal().dot(pos.faces[0].face_normal()), -1.0)
 
     # If an invalid direction is given, it should just default to the positive direction
-    invalid_neg = design.extrude_sketch("invalid_negative_face", sk, 10, direction="z")
+    invalid_neg = design.extrude_face("invalid_negative_face", body.faces[0], 10, direction="z")
     assert invalid_neg.faces[0].face_normal() == pos.faces[0].face_normal()
     assert np.isclose(invalid_neg.faces[0].face_normal().dot(pos.faces[0].face_normal()), 1.0)
 
