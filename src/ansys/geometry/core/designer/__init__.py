e4508414b8fe157fbfd5cf2565e5ba305fe5ef46
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2024-03-07 12:19:25 +0100

    feat: add extrude in pos/neg direction (#1042)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>

--- src/ansys/geometry/core/designer/__init__.py
+++ src/ansys/geometry/core/designer/__init__.py
@@ -22,7 +22,7 @@
 """PyAnsys Geometry designer subpackage."""
 
 from ansys.geometry.core.designer.body import Body, MasterBody, MidSurfaceOffsetType
-from ansys.geometry.core.designer.component import Component, SharedTopologyType
+from ansys.geometry.core.designer.component import Component, ExtrusionDirection, SharedTopologyType
 from ansys.geometry.core.designer.design import Design, DesignFileFormat
 from ansys.geometry.core.designer.designpoint import DesignPoint
 from ansys.geometry.core.designer.edge import CurveType, Edge
