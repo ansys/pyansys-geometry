77fc78192c85839bb6f306a0c556ea002f2c4c94
m-bini <matteo.bini@ansys.com>
2024-03-22 17:31:50 +0100

    Update test_design_import.py

--- tests/integration/test_design_import.py
+++ tests/integration/test_design_import.py
@@ -258,4 +258,21 @@ def test_design_insert(modeler: Modeler):
     # Check that there are two components
     assert len(design.components) == 2
     assert design.components[0].name == "Component_Cylinder"
-    assert design.components[1].name == "DuplicateFacesDesignBefore"
+    assert design.components[1].name == "DuplicatesDesign"
+
+def test_design_insert_with_import(modeler: Modeler):
+    """Test inserting a file into the design through the external format import process."""
+    # Create a design and sketch a circle
+    design = modeler.create_design("Insert")
+    sketch = Sketch()
+    sketch.circle(Point2D([0, 0]), 10)
+    comp = design.add_component("Component_Cylinder")
+    comp.extrude_sketch("Body_Cylinder", sketch, 5)
+
+    # Import and insert a different file
+    design.insert_file("./tests/integration/files/import/catia_car/Wheel1.CATPart")
+
+    # Check that there are two components
+    assert len(design.components) == 2
+    assert design.components[0].name == "Component_Cylinder"
+    assert design.components[1].name == "Wheel1"
