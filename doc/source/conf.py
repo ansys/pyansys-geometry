bd184aa0e9c4b534e042cc560e1baf32e7ed6f7f
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2024-03-25 09:28:12 +0100

    temp: comment out template

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -194,7 +194,7 @@ autoapi_options = [
     "show-module-summary",
     "special-members",
 ]
-autoapi_template_dir = get_autoapi_templates_dir_relative_path(Path(__file__))
+# autoapi_template_dir = get_autoapi_templates_dir_relative_path(Path(__file__))
 suppress_warnings = ["autoapi.python_import_resolution", "design.grid"]
 autoapi_python_use_implicit_namespaces = True
 autoapi_keep_files = True
