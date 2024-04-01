77b8ffe8600e3ec0cc1a111ebb8a7a5310e5366a
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2024-04-01 11:24:00 +0200

    Update doc/make.bat

--- doc/make.bat
+++ doc/make.bat
@@ -8,7 +8,7 @@ if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
 if "%SPHINXOPTS%" == "" (
-	set SPHINXOPTS=-j auto --color
+	set SPHINXOPTS=-j auto -W --color
 )
 set SOURCEDIR=source
 set APIDIR=api
