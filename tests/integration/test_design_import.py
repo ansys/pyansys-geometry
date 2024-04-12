c444432169584dfcbaa5c8c61cb2e9e7bb000eaa
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2024-04-12 08:58:53 +0200

    Apply suggestions from code review

--- tests/integration/test_design_import.py
+++ tests/integration/test_design_import.py
@@ -208,8 +208,6 @@ def test_open_file(modeler: Modeler, tmp_path_factory: pytest.TempPathFactory):
 
         # IGES
 
-        # TODO: Something has gone wrong with IGES
-        # TODO: Issue https://github.com/ansys/pyansys-geometry/issues/801
 
         file = tmp_path_factory.mktemp("test_design_import") / "two_cars.igs"
         design.download(file, DesignFileFormat.IGES)
