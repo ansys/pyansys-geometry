2722049cebcbfcb16e68c0b9389d906407f5f086
Alex Fernandez <21alex295@gmail.com>
2024-05-29 08:12:38 -0400

    fix: Interactive documentation (#1226)
    Co-authored-by: pyansys-ci-bot <pyansys.github.bot@ansys.com>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -21,7 +21,7 @@ from sphinx.builders.latex import LaTeXBuilder
 
 from ansys.geometry.core import __version__
 
-os.environ["PYANSYS_GEOMETRY_DOC_BUILD"] = "true"
+os.environ["PYANSYS_VISUALIZER_DOC_MODE"] = "true"
 
 LaTeXBuilder.supported_image_types = ["image/png", "image/pdf", "image/svg+xml"]
 
