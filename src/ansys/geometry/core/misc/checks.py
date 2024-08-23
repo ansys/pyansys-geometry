a397f63524ed88233553ff5c163bac06b70f83fd
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2024-08-23 15:59:13 +0200

    chore: solving various warnings (#1368)
    Co-authored-by: pyansys-ci-bot <92810346+pyansys-ci-bot@users.noreply.github.com>

--- src/ansys/geometry/core/misc/checks.py
+++ src/ansys/geometry/core/misc/checks.py
@@ -21,7 +21,8 @@
 # SOFTWARE.
 """Provides functions for performing common checks."""
 
-from typing import TYPE_CHECKING, Iterable
+from collections.abc import Iterable
+from typing import TYPE_CHECKING
 import warnings
 
 import numpy as np
