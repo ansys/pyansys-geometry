ed7dda8da6cf0a74c546e270d4bb21477d4445f5
Umut Soysal <97195410+umutsoysalansys@users.noreply.github.com>
2024-09-03 08:32:23 -0500

    Update tests/integration/test_design.py
    Co-authored-by: Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>

--- tests/integration/test_design.py
+++ tests/integration/test_design.py
@@ -382,7 +382,7 @@ def test_add_component_with_instance_name(modeler: Modeler):
     nested_component = design.add_component("NestedComponent")
     nested_component2 = design.add_component("NestedComponent2", instance_name="first instance")
 
-    assert nested_component._intance_name is None
+    assert nested_component.instance_name is None
     assert nested_component2._intance_name == "first instance"
 
 
