3fa56d4710228abe8694d6d22bf46e52598236fa
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2024-09-17 14:21:58 +0200

    feat: update conf.py to include new hooks

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -430,6 +430,28 @@ def convert_notebooks_to_scripts(app: sphinx.application.Sphinx, exception):
         else:
             logger.info(f"Converted {count} notebooks to scripts")
 
+def replace_version_in_qmd(file_path, search, replace):
+    """Update the version in cheatsheet."""
+    with open(file_path, "r") as file:
+        content = file.read()
+
+    logger.info(f"replace_version_in_qmd: replacing {search} with {replace}")
+    content = content.replace(f"version: {search}", f"version: {replace}")
+
+    with open(file_path, "w") as file:
+        file.write(content)
+
+def update_qmd_mod(app: sphinx.application.Sphinx):
+    """Update the version in cheatsheet."""
+    cheathseet_path = Path(__file__).parent / "cheatsheet" / "cheat_sheet.qmd"
+    logger.info(f"Changing {cheathseet_path}")
+    replace_version_in_qmd(cheathseet_path, "main", version)
+
+def revert_qmd_mod(app: sphinx.application.Sphinx, exception):
+    """Revert the version in cheatsheet that was modified."""
+    cheathseet_path = Path(__file__).parent / "cheatsheet" / "cheat_sheet.qmd"
+    logger.info(f"Reverting {cheathseet_path}")
+    replace_version_in_qmd(cheathseet_path, version, "main")
 
 def setup(app: sphinx.application.Sphinx):
     """Run different hook functions during the documentation build.
@@ -440,21 +462,14 @@ def setup(app: sphinx.application.Sphinx):
         Sphinx instance containing all the configuration for the documentation build.
     """
     logger.info("Configuring Sphinx hooks...")
+    
+    # At the beginning of the build process - udpate the version in cheatsheet
+    app.connect("builder-inited", update_qmd_mod)
+    
     if BUILD_EXAMPLES:
         # Run at the end of the build process
         logger.info("Connecting build-finished hook for converting notebooks to scripts...")
         app.connect("build-finished", convert_notebooks_to_scripts)
-
-
-def replace_version_in_qmd(file_path, version):
-    """Update the version in cheatsheet."""
-    with open(file_path, "r") as file:
-        content = file.read()
-
-    content = content.replace("version: main", f"version: {version}")
-
-    with open(file_path, "w") as file:
-        file.write(content)
-
-
-replace_version_in_qmd(Path(__file__).parent / "cheatsheet" / "cheat_sheet.qmd", version)
+    
+    # Reverting the version in cheatsheet
+    app.connect("build-finished", revert_qmd_mod)
