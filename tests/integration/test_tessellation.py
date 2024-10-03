181e0ded250cf0750e934feea4b1c9b95b8c3f3c
m-bini <matteo.bini@ansys.com>
2024-10-03 16:59:12 +0200

    Update test_tessellation.py

--- tests/integration/test_tessellation.py
+++ tests/integration/test_tessellation.py
@@ -69,11 +69,19 @@ def test_body_tessellate(modeler: Modeler):
     blocks_2 = body_2.tessellate()
     assert "MultiBlock" in str(blocks_2)
     assert blocks_2.n_blocks == 3
-    assert blocks_2.bounds == pytest.approx(
-        [0.019999999999999997, 0.04, 0.019999999999999886, 0.040000000000000036, 0.0, 0.03],
-        rel=1e-6,
-        abs=1e-8,
-    )
+    if modeler.client.backend_type != BackendType.LINUX_SERVICE:
+        assert blocks_2.bounds == pytest.approx(
+            [0.019999999999999997, 0.04, 0.020151922469877917, 0.03984807753012208, 0.0, 0.03],
+            rel=1e-6,
+            abs=1e-8,
+        )
+    else:
+        assert blocks_2.bounds == pytest.approx(
+            [0.019999999999999997, 0.04, 0.019999999999999886, 0.040000000000000036, 0.0, 0.03],
+            rel=1e-6,
+            abs=1e-8,
+        )
+
     assert blocks_2.center == pytest.approx([0.03, 0.03, 0.015],rel=1e-6,abs=1e-8)
 
     # Tessellate the body merging the individual faces
@@ -129,11 +137,18 @@ def test_component_tessellate(modeler: Modeler):
     assert "MultiBlock" in str(dataset)
     assert dataset.n_blocks == 1
     assert dataset.center == pytest.approx([0.0, 0.0, 10.0],rel=1e-6,abs=1e-8)
-    assert dataset.bounds == pytest.approx(
-        [-25.0, 25.0, -25.000000000000277, 25.000000000000096, 0.0, 20.0],
-        rel=1e-6,
-        abs=1e-8,
-    )
+    if modeler.client.backend_type != BackendType.LINUX_SERVICE:
+        assert dataset.bounds == pytest.approx(
+            [-25.0, 25.0, -24.999251562526105, 24.999251562526105, 0.0, 20.0],
+            rel=1e-6,
+            abs=1e-8,
+        )
+    else:
+        assert dataset.bounds == pytest.approx(
+            [-25.0, 25.0, -25.000000000000277, 25.000000000000096, 0.0, 20.0],
+            rel=1e-6,
+            abs=1e-8,
+        )
 
     # Tessellate the component by merging it to a single dataset.
     mesh = comp.tessellate(merge_component=True)
