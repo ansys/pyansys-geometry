0746e2d9ffded349130966697cf866cfa69b9d45
Jorge Martínez <28702884+jorgepiloto@users.noreply.github.com>
2024-10-17 08:37:19 +0200

    docs: avoid having a drop down in the top navigation bar (#1485)
    Co-authored-by: pyansys-ci-bot <92810346+pyansys-ci-bot@users.noreply.github.com>
    Co-authored-by: Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -135,6 +135,7 @@ html_context = {
     "doc_path": "doc/source",
 }
 html_theme_options = {
+    "header_links_before_dropdown": 7,
     "logo": "pyansys",
     "switcher": {
         "json_url": f"https://{cname}/versions.json",
