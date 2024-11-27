70b0a795877bc83f7ea7eb8a4288e6828cc2145e
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2024-11-27 11:23:11 +0100

    build: bump main Python version and intersphinx Python version

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -196,7 +196,7 @@ extensions = [
 
 # Intersphinx mapping
 intersphinx_mapping = {
-    "python": ("https://docs.python.org/3.11", None),
+    "python": ("https://docs.python.org/3.13", None),
     "numpy": ("https://numpy.org/doc/stable", None),
     "scipy": ("https://docs.scipy.org/doc/scipy", None),
     "pyvista": ("https://docs.pyvista.org", None),
