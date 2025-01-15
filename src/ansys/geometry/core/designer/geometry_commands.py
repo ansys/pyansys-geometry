eae158e69b5521d0e8606d15c2e41ce6b3018054
jkerstet <jacob.kerstetter@ansys.com>
2025-01-15 09:30:51 -0500

    wip

--- src/ansys/geometry/core/designer/geometry_commands.py
+++ src/ansys/geometry/core/designer/geometry_commands.py
@@ -30,6 +30,7 @@ from ansys.api.geometry.v0.commands_pb2 import (
     ExtrudeFacesUpToRequest,
     FilletRequest,
     FullFilletRequest,
+    RenameObjectRequest
 )
 from ansys.api.geometry.v0.commands_pb2_grpc import CommandsStub
 from ansys.geometry.core.connection import GrpcClient
