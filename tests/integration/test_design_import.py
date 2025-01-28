5d6781ca9da4fd441e6390764ab1fb671be49a06
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2025-01-28 11:37:45 +0100

    fix: cleanup

--- tests/integration/test_design_import.py
+++ tests/integration/test_design_import.py
@@ -214,12 +214,7 @@ def test_open_file(modeler: Modeler, tmp_path_factory: pytest.TempPathFactory):
     if modeler.client.backend_type == BackendType.LINUX_SERVICE:
         modeler.unsupported.set_export_id(base_body.id, PersistentIdType.PRIME_ID, "1")
         modeler.unsupported.set_export_id(wheel_body.id, PersistentIdType.PRIME_ID, "2")
-    if modeler.client.backend_type == BackendType.LINUX_SERVICE:
-        modeler.unsupported.set_export_id(base_body.id, PersistentIdType.PRIME_ID, "1")
-        modeler.unsupported.set_export_id(wheel_body.id, PersistentIdType.PRIME_ID, "2")
 
-        modeler.unsupported.set_export_id(base_body.faces[0].id, PersistentIdType.PRIME_ID, "3")
-        modeler.unsupported.set_export_id(base_body.edges[0].id, PersistentIdType.PRIME_ID, "4")
         modeler.unsupported.set_export_id(base_body.faces[0].id, PersistentIdType.PRIME_ID, "3")
         modeler.unsupported.set_export_id(base_body.edges[0].id, PersistentIdType.PRIME_ID, "4")
 
@@ -229,18 +224,9 @@ def test_open_file(modeler: Modeler, tmp_path_factory: pytest.TempPathFactory):
         bodies2 = modeler.unsupported.get_body_occurrences_from_import_id(
             "2", PersistentIdType.PRIME_ID
         )
-        bodies1 = modeler.unsupported.get_body_occurrences_from_import_id(
-            "1", PersistentIdType.PRIME_ID
-        )
-        bodies2 = modeler.unsupported.get_body_occurrences_from_import_id(
-            "2", PersistentIdType.PRIME_ID
-        )
 
-        # requires a change to core service, uncomment on next core service update
         assert base_body.id in [b.id for b in bodies1]
         assert wheel_body.id in [b.id for b in bodies2]
-        # assert base_body.id not in [b.id for b in bodies1]
-        # assert wheel_body.id not in [b.id for b in bodies2]
 
         faces = modeler.unsupported.get_face_occurrences_from_import_id(
             "3", PersistentIdType.PRIME_ID
@@ -248,18 +234,9 @@ def test_open_file(modeler: Modeler, tmp_path_factory: pytest.TempPathFactory):
         edges = modeler.unsupported.get_edge_occurrences_from_import_id(
             "4", PersistentIdType.PRIME_ID
         )
-        faces = modeler.unsupported.get_face_occurrences_from_import_id(
-            "3", PersistentIdType.PRIME_ID
-        )
-        edges = modeler.unsupported.get_edge_occurrences_from_import_id(
-            "4", PersistentIdType.PRIME_ID
-        )
 
-        # requires a change to core service, uncomment on next core service update
         assert base_body.faces[0].id in [f.id for f in faces]
         assert base_body.edges[0].id in [e.id for e in edges]
-        # assert base_body.faces[0].id not in [f.id for f in faces]
-        # assert base_body.edges[0].id not in [e.id for e in edges]
 
     file = tmp_path_factory.mktemp("test_design_import") / "two_cars.scdocx"
     design.download(str(file))
