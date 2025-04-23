53c09b34746e00e3b8c953373a63dfa919000941
Umut Soysal <umut.soysal@ansys.com>
2025-04-23 13:02:33 -0500

    bs

--- src/ansys/geometry/core/_grpc/_services/v1/repair_tools.py
+++ src/ansys/geometry/core/_grpc/_services/v1/repair_tools.py
@@ -19,7 +19,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-"""Module containing the repair tools service implementation (abstraction layer)."""
+"""Module containing the repair tools service implementation."""
 
 from abc import ABC
 
