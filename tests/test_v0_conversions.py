15b9b4868533170871277cd4e11b1cd546d11c96
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2025-10-20 10:39:13 +0200

    chore: cleanup conversions module and remaining imports

--- tests/test_v0_conversions.py
+++ tests/test_v0_conversions.py
@@ -30,39 +30,51 @@ from ansys.api.geometry.v0.models_pb2 import (
     Surface as GRPCSurface,
     SurfaceType as GRPCSurfaceType,
 )
+import numpy as np
 from pint import Quantity
 import pytest
 
 from ansys.geometry.core._grpc._services.v0.conversions import (
     from_curve_to_grpc_curve,
     from_design_file_format_to_grpc_part_export_format,
+    from_frame_to_grpc_frame,
     from_grpc_backend_type_to_backend_type,
     from_grpc_curve_to_curve,
     from_grpc_material_property_to_material_property,
+    from_grpc_material_to_material,
     from_grpc_surface_to_surface,
+    from_plane_to_grpc_plane,
+    from_point2d_to_grpc_point,
+    from_point3d_to_grpc_point,
+    from_sketch_arc_to_grpc_arc,
+    from_sketch_circle_to_grpc_circle,
+    from_sketch_ellipse_to_grpc_ellipse,
+    from_sketch_polygon_to_grpc_polygon,
+    from_sketch_segment_to_grpc_line,
     from_sketch_shapes_to_grpc_geometries,
     from_surface_to_grpc_surface,
     from_tess_options_to_grpc_tess_options,
+    from_unit_vector_to_grpc_direction,
 )
 from ansys.geometry.core.connection.backend import BackendType
-from ansys.geometry.core.connection.conversions import (
-    curve_to_grpc_curve,
-    grpc_curve_to_curve,
-    grpc_material_property_to_material_property,
-    grpc_material_to_material,
-    grpc_surface_to_surface,
-    sketch_shapes_to_grpc_geometries,
-)
 from ansys.geometry.core.designer.design import DesignFileFormat
 from ansys.geometry.core.designer.face import SurfaceType
 from ansys.geometry.core.materials.material import MaterialPropertyType
-from ansys.geometry.core.math import Plane, Point2D, Point3D, UnitVector3D
-from ansys.geometry.core.misc.measurements import DEFAULT_UNITS, UNITS
+from ansys.geometry.core.math.frame import Frame
+from ansys.geometry.core.math.plane import Plane
+from ansys.geometry.core.math.point import Point2D, Point3D
+from ansys.geometry.core.math.vector import UnitVector3D
+from ansys.geometry.core.misc.measurements import DEFAULT_UNITS, Angle
 from ansys.geometry.core.misc.options import TessellationOptions
-from ansys.geometry.core.shapes.curves import Circle, Curve, Ellipse, Line
-from ansys.geometry.core.shapes.curves.nurbs import NURBSCurve
+from ansys.geometry.core.misc.units import UNITS
+from ansys.geometry.core.shapes.curves import Circle, Curve, Ellipse, Line, NURBSCurve
 from ansys.geometry.core.shapes.surfaces import Cone, Cylinder, PlaneSurface, Sphere, Torus
-from ansys.geometry.core.sketch import Arc, Polygon, SketchCircle, SketchEdge, SketchEllipse
+from ansys.geometry.core.sketch.arc import Arc
+from ansys.geometry.core.sketch.circle import SketchCircle
+from ansys.geometry.core.sketch.edge import SketchEdge
+from ansys.geometry.core.sketch.ellipse import SketchEllipse
+from ansys.geometry.core.sketch.polygon import Polygon
+from ansys.geometry.core.sketch.segment import SketchSegment
 
 
 def test_sketch_shapes_to_grpc_geometries_multiple_faces():
@@ -75,7 +87,7 @@ def test_sketch_shapes_to_grpc_geometries_multiple_faces():
     faces = [circle, ellipse, polygon]
 
     # Call the function
-    geometries = sketch_shapes_to_grpc_geometries(plane, edges=[], faces=faces)
+    geometries = from_sketch_shapes_to_grpc_geometries(plane, edges=[], faces=faces)
 
     # Assert that the circle, ellipse, and polygon were converted and added
     assert len(geometries.circles) == 1
@@ -91,7 +103,7 @@ def test_sketch_shapes_to_grpc_geometries_one_curve_lines():
     edges = [edge]
 
     # Call the function with only_one_curve=True
-    one_curve_geometry = sketch_shapes_to_grpc_geometries(
+    one_curve_geometry = from_sketch_shapes_to_grpc_geometries(
         plane, edges=edges, faces=[], only_one_curve=True
     )
 
@@ -107,7 +119,7 @@ def test_sketch_shapes_to_grpc_geometries_one_curve_arcs():
     edges = [arc]
 
     # Call the function with only_one_curve=True
-    one_curve_geometry = sketch_shapes_to_grpc_geometries(
+    one_curve_geometry = from_sketch_shapes_to_grpc_geometries(
         plane, edges=edges, faces=[], only_one_curve=True
     )
 
@@ -123,7 +135,7 @@ def test_sketch_shapes_to_grpc_geometries_one_curve_circles():
     faces = [circle]
 
     # Call the function with only_one_curve=True
-    one_curve_geometry = sketch_shapes_to_grpc_geometries(
+    one_curve_geometry = from_sketch_shapes_to_grpc_geometries(
         plane, edges=[], faces=faces, only_one_curve=True
     )
 
@@ -141,7 +153,7 @@ def test_sketch_shapes_to_grpc_geometries_one_curve_ellipses():
     faces = [ellipse]
 
     # Call the function with only_one_curve=True
-    one_curve_geometry = sketch_shapes_to_grpc_geometries(
+    one_curve_geometry = from_sketch_shapes_to_grpc_geometries(
         plane, edges=[], faces=faces, only_one_curve=True
     )
 
@@ -164,7 +176,7 @@ def test_sketch_shapes_to_grpc_geometries_one_curve_polygons():
     faces = [polygon]
 
     # Call the function with only_one_curve=True
-    one_curve_geometry = sketch_shapes_to_grpc_geometries(
+    one_curve_geometry = from_sketch_shapes_to_grpc_geometries(
         plane, edges=[], faces=faces, only_one_curve=True
     )
 
@@ -266,7 +278,7 @@ def test_grpc_surface_to_surface(surface_type, surface_data, expected_type):
     )()
 
     # Call the function
-    result = grpc_surface_to_surface(surface, surface_type)
+    result = from_grpc_surface_to_surface(surface, surface_type)
 
     # Assert the result type
     if expected_type is None:
@@ -339,7 +351,7 @@ def test_grpc_curve_to_curve(curve_data, expected_type, expected_attributes):
     )
 
     # Call the function
-    result = grpc_curve_to_curve(grpc_curve)
+    result = from_grpc_curve_to_curve(grpc_curve)
 
     # Assert the result type
     if expected_type is None:
@@ -367,7 +379,7 @@ def test_curve_to_grpc_curve_ellipse():
     ellipse = Ellipse(origin, major_radius, minor_radius, reference, axis)
 
     # Call the function
-    grpc_curve = curve_to_grpc_curve(ellipse)
+    grpc_curve = from_curve_to_grpc_curve(ellipse)
 
     # Assert the GRPCCurve attributes
     assert grpc_curve.origin.x == origin.x.m_as(UNITS.m)
@@ -406,7 +418,7 @@ def test_grpc_material_to_material():
     )
 
     # Call the function
-    material = grpc_material_to_material(grpc_material)
+    material = from_grpc_material_to_material(grpc_material)
 
     # Assert the Material attributes
     assert material.name == "Steel"
@@ -427,7 +439,7 @@ def test_grpc_material_property_to_material_property_invalid_units():
     )
 
     # Call the function
-    material_property = grpc_material_property_to_material_property(invalid_units_property)
+    material_property = from_grpc_material_property_to_material_property(invalid_units_property)
 
     # Assert fallback logic
     assert material_property.type == "Density"
@@ -785,3 +797,256 @@ def test_from_design_file_format_to_grpc_part_export_format_none():
 
     # Assert that the result is None
     assert export_format is None
+
+
+def test_circle_message_conversion():
+    """Test conversion between :class: `SketchCircle
+    <ansys.geometry.core.sketch.circle.SketchCircle>` and expected gRPC message
+    type.
+    """
+    circle = SketchCircle(
+        Point2D([10, 100], UNITS.mm),
+        Quantity(300, UNITS.mm),
+    )
+    grpc_circle_message = from_sketch_circle_to_grpc_circle(
+        circle, Plane(Point3D([10, 100, 1000], UNITS.mm))
+    )
+
+    assert grpc_circle_message.center.x == 0.02
+    assert grpc_circle_message.center.y == 0.2
+    assert grpc_circle_message.center.z == 1.0
+    assert grpc_circle_message.radius == 0.3
+
+
+def test_ellipse_message_conversion():
+    """Test conversion between :class:`SketchEllipse <
+    ansys.geometry.core.sketch.ellipse.SketchEllipse>` and expected gRPC
+    message type.
+    """
+    ellipse = SketchEllipse(
+        Point2D([10, 100], UNITS.mm),
+        Quantity(300, UNITS.mm),
+        Quantity(50, UNITS.mm),
+    )
+    grpc_ellipse_message = from_sketch_ellipse_to_grpc_ellipse(
+        ellipse, Plane(Point3D([10, 100, 1000], UNITS.mm))
+    )
+
+    assert grpc_ellipse_message.center.x == 0.02
+    assert grpc_ellipse_message.center.y == 0.2
+    assert grpc_ellipse_message.center.z == 1.0
+    assert grpc_ellipse_message.majorradius == 0.3
+    assert grpc_ellipse_message.minorradius == 0.05
+    assert grpc_ellipse_message.angle == 0
+
+    rotated_ellipse = SketchEllipse(
+        Point2D([10, 100], UNITS.mm),
+        Quantity(300, UNITS.mm),
+        Quantity(50, UNITS.mm),
+        angle=Angle(45, UNITS.degrees),
+    )
+    rotated_grpc_ellipse_message = from_sketch_ellipse_to_grpc_ellipse(
+        rotated_ellipse, Plane(Point3D([10, 100, 1000], UNITS.mm))
+    )
+
+    assert rotated_grpc_ellipse_message.center.x == 0.02
+    assert rotated_grpc_ellipse_message.center.y == 0.2
+    assert rotated_grpc_ellipse_message.center.z == 1.0
+    assert rotated_grpc_ellipse_message.majorradius == 0.3
+    assert rotated_grpc_ellipse_message.minorradius == 0.05
+    assert rotated_grpc_ellipse_message.angle == np.pi / 4
+
+
+def test_segment_message_conversion():
+    """Test conversion between :class: `SketchSegment
+    <ansys.geometry.core.sketch.segment.SketchSegment>` and expected gRPC
+    message type.
+    """
+    segment = SketchSegment(
+        Point2D([30, 400], UNITS.mm),
+        Point2D([500, 600], UNITS.mm),
+    )
+    grpc_line_message = from_sketch_segment_to_grpc_line(
+        segment, Plane(Point3D([10, 100, 1000], UNITS.mm))
+    )
+
+    assert grpc_line_message.start.x == 0.04
+    assert grpc_line_message.start.y == 0.5
+    assert grpc_line_message.start.z == 1.0
+    assert grpc_line_message.end.x == 0.51
+    assert grpc_line_message.end.y == pytest.approx(0.7000000000000001, 1e-8, 1e-8)
+    assert grpc_line_message.end.z == 1.0
+
+
+def test_polygon_message_conversion():
+    """Test conversion between :class:`Polygon
+    <ansys.geometry.core.sketch.polygon.Polygon>` and expected gRPC message
+    type.
+    """
+    polygon = Polygon(
+        Point2D([10, 100], UNITS.mm),
+        Quantity(300, UNITS.mm),
+        5,
+    )
+    grpc_polygon_message = from_sketch_polygon_to_grpc_polygon(
+        polygon, Plane(Point3D([10, 100, 1000], UNITS.mm))
+    )
+
+    assert grpc_polygon_message.center.x == 0.02
+    assert grpc_polygon_message.center.y == 0.2
+    assert grpc_polygon_message.center.z == 1.0
+    assert grpc_polygon_message.radius == 0.3
+    assert grpc_polygon_message.numberofsides == 5
+    assert grpc_polygon_message.angle == 0
+
+    rotated_polygon = Polygon(
+        Point2D([10, 100], UNITS.mm), Quantity(300, UNITS.mm), 5, angle=Angle(45, UNITS.degrees)
+    )
+    rotated_grpc_polygon_message = from_sketch_polygon_to_grpc_polygon(
+        rotated_polygon, Plane(Point3D([10, 100, 1000], UNITS.mm))
+    )
+
+    assert rotated_grpc_polygon_message.center.x == 0.02
+    assert rotated_grpc_polygon_message.center.y == 0.2
+    assert rotated_grpc_polygon_message.center.z == 1.0
+    assert rotated_grpc_polygon_message.radius == 0.3
+    assert rotated_grpc_polygon_message.numberofsides == 5
+    assert rotated_grpc_polygon_message.angle == np.pi / 4
+
+
+def test_point3d_message_conversion():
+    """Test conversion between :class:`Point3D
+    <ansys.geometry.core.math.point.Point3D>` and expected gRPC message
+    type.
+    """
+    point = Point3D([10, 100, 1000], UNITS.mm)
+    grpc_point_message = from_point3d_to_grpc_point(point)
+
+    assert grpc_point_message.x == 0.01
+    assert grpc_point_message.y == 0.1
+    assert grpc_point_message.z == 1.0
+
+
+def test_point2d_message_conversion():
+    """Test conversion between :class:`Point2D` and expected gRPC message
+    type.
+    """
+    point = Point2D([10, 100], UNITS.mm)
+    grpc_point_message = from_point2d_to_grpc_point(
+        Plane(Point3D([10, 100, 1000], UNITS.mm)), point
+    )
+
+    assert grpc_point_message.x == 0.02
+    assert grpc_point_message.y == 0.2
+    assert grpc_point_message.z == 1.0
+
+
+def test_unit_vector_message_conversion():
+    """Test conversion between :class:`UnitVector3D
+    <ansys.geometry.core.math.vector.unitVector3D>` and expected gRPC message
+    type.
+    """
+    unit_vector = UnitVector3D([1, 1, 1])
+    grpc_unit_vector_message = from_unit_vector_to_grpc_direction(unit_vector)
+
+    assert grpc_unit_vector_message.x == 0.5773502691896258
+    assert grpc_unit_vector_message.y == 0.5773502691896258
+    assert grpc_unit_vector_message.z == 0.5773502691896258
+
+
+def test_arc_message_conversion():
+    """Test conversion between :class:`Arc
+    <ansys.geometry.core.sketch.arc.Arc>` and expected gRPC message type.
+    """
+    arc = Arc(
+        Point2D([100, 400], UNITS.mm),
+        Point2D([900, 800], UNITS.mm),
+        Point2D([500, 600], UNITS.mm),
+    )
+    grpc_arc_message = from_sketch_arc_to_grpc_arc(arc, Plane(Point3D([10, 100, 1000], UNITS.mm)))
+
+    assert grpc_arc_message.center.x == 0.51
+    assert grpc_arc_message.center.y == pytest.approx(0.7000000000000001, 1e-8, 1e-8)
+    assert grpc_arc_message.center.z == 1.0
+
+    assert grpc_arc_message.start.x == 0.11
+    assert grpc_arc_message.start.y == 0.5
+    assert grpc_arc_message.start.z == 1.0
+
+    assert grpc_arc_message.end.x == 0.91
+    assert grpc_arc_message.end.y == 0.9
+    assert grpc_arc_message.end.z == 1.0
+
+    assert grpc_arc_message.axis.x == 0
+    assert grpc_arc_message.axis.y == 0
+    assert grpc_arc_message.axis.z == 1
+
+    arc2 = Arc(
+        Point2D([200, 500], UNITS.mm),
+        Point2D([1000, 900], UNITS.mm),
+        Point2D([600, 700], UNITS.mm),
+        True,
+    )
+    grpc_arc_message2 = from_sketch_arc_to_grpc_arc(arc2, Plane(Point3D([10, 100, 1000], UNITS.mm)))
+
+    assert grpc_arc_message2.center.x == 0.61
+    assert grpc_arc_message2.center.y == pytest.approx(0.8000000000000002, 1e-8, 1e-8)
+    assert grpc_arc_message2.center.z == 1.0
+
+    assert grpc_arc_message2.start.x == pytest.approx(0.21000000000000002, 1e-8, 1e-8)
+    assert grpc_arc_message2.start.y == 0.6
+    assert grpc_arc_message2.start.z == 1.0
+
+    assert grpc_arc_message2.end.x == 1.01
+    assert grpc_arc_message2.end.y == 1.0
+    assert grpc_arc_message2.end.z == 1.0
+
+    assert grpc_arc_message2.axis.x == 0
+    assert grpc_arc_message2.axis.y == 0
+    assert grpc_arc_message2.axis.z == -1
+
+
+def test_plane_message_conversion():
+    """Test conversion between :class:`Plane
+    <ansys.geometry.core.math.plane.Plane>` and expected gRPC message type.
+    """
+    plane = Plane(
+        Point3D([10, 200, 3000], UNITS.mm), UnitVector3D([1, 1, 0]), UnitVector3D([1, -1, 0])
+    )
+    grpc_plane_message = from_plane_to_grpc_plane(plane)
+
+    assert grpc_plane_message.frame.origin.x == 0.01
+    assert grpc_plane_message.frame.origin.y == 0.2
+    assert grpc_plane_message.frame.origin.z == 3.0
+
+    assert grpc_plane_message.frame.dir_x.x == pytest.approx(0.7071067811865475, rel=1e-7, abs=1e-8)
+    assert grpc_plane_message.frame.dir_x.y == pytest.approx(0.7071067811865475, rel=1e-7, abs=1e-8)
+    assert grpc_plane_message.frame.dir_x.z == 0.0
+
+    assert grpc_plane_message.frame.dir_y.x == pytest.approx(0.7071067811865475, rel=1e-7, abs=1e-8)
+    assert grpc_plane_message.frame.dir_y.y == pytest.approx(
+        -0.7071067811865475, rel=1e-7, abs=1e-8
+    )
+    assert grpc_plane_message.frame.dir_y.z == 0.0
+
+
+def test_frame_message_conversion():
+    """Test conversion between :class:`Frame
+    <ansys.geometry.core.math.frame.Frame>` and expected gRPC message type.
+    """
+    frame = Frame(
+        Point3D([10, 200, 3000], UNITS.mm), UnitVector3D([1, 1, 0]), UnitVector3D([1, -1, 0])
+    )
+    grpc_frame_message = from_frame_to_grpc_frame(frame)
+
+    assert grpc_frame_message.origin.x == 0.01
+    assert grpc_frame_message.origin.y == 0.2
+    assert grpc_frame_message.origin.z == 3.0
+
+    assert grpc_frame_message.dir_x.x == pytest.approx(0.7071067811865475, rel=1e-7, abs=1e-8)
+    assert grpc_frame_message.dir_x.y == pytest.approx(0.7071067811865475, rel=1e-7, abs=1e-8)
+    assert grpc_frame_message.dir_x.z == 0.0
+
+    assert grpc_frame_message.dir_y.x == pytest.approx(0.7071067811865475, rel=1e-7, abs=1e-8)
+    assert grpc_frame_message.dir_y.y == pytest.approx(-0.7071067811865475, rel=1e-7, abs=1e-8)
+    assert grpc_frame_message.dir_y.z == 0.0
