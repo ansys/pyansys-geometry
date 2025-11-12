1e20fa257c9a569da87a0fbe83cad1b1e52a9a96
Jacob Kerstetter <83893427+jacobrkerstetter@users.noreply.github.com>
2025-11-12 08:21:36 -0500

    fix: add axis for circular pattern (#2367)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: pyansys-ci-bot <92810346+pyansys-ci-bot@users.noreply.github.com>
    Co-authored-by: Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>

--- src/ansys/geometry/core/designer/geometry_commands.py
+++ src/ansys/geometry/core/designer/geometry_commands.py
@@ -716,7 +716,7 @@ class GeometryCommands:
     def create_circular_pattern(
         self,
         selection: Union["Face", list["Face"]],
-        circular_axis: "Edge",
+        circular_axis: Union["Edge", Line],
         circular_count: int,
         circular_angle: Angle | Quantity | Real,
         two_dimensional: bool = False,
@@ -755,6 +755,7 @@ class GeometryCommands:
         --------
         This method is only available starting on Ansys release 25R2.
         """
+        from ansys.geometry.core.designer.edge import Edge
         from ansys.geometry.core.designer.face import Face
 
         selection: list[Face] = selection if isinstance(selection, list) else [selection]
@@ -778,16 +779,25 @@ class GeometryCommands:
                     "a two-dimensional pattern is desired."
                 )
             )
+        if self._grpc_client.backend_version < (26, 1, 0) and isinstance(circular_axis, Line):
+            raise ValueError(
+                (
+                    "Using a Line as the circular axis is only supported "
+                    "starting in Ansys release 26R1."
+                )
+            )
 
         # Convert angle and pitch to appropriate objects
         if not isinstance(circular_angle, Angle):
             circular_angle = Angle(circular_angle)
         if linear_pitch is not None and not isinstance(linear_pitch, Distance):
             linear_pitch = Distance(linear_pitch)
+        if isinstance(circular_axis, Edge):
+            circular_axis = circular_axis.id
 
         result = self._grpc_client.services.patterns.create_circular_pattern(
             selection_ids=[object.id for object in selection],
-            circular_axis_id=circular_axis.id,
+            circular_axis=circular_axis,
             circular_count=circular_count,
             circular_angle=circular_angle,
             two_dimensional=two_dimensional,
