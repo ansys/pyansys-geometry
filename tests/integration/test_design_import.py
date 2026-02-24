296778a1b5eead23d2fd190719301d645eca1cfc
Jacob Kerstetter <jacob.kerstetter@ansys.com>
2026-02-24 15:02:01 -0500

    test: update asserts

--- tests/integration/test_design_import.py
+++ tests/integration/test_design_import.py
@@ -509,9 +509,11 @@ def test_design_import_pmdb(modeler: Modeler):
     """Test importing a PMDB file."""
     # Open the design
     design = modeler.open_file(Path(IMPORT_FILES_DIR, "PMDB/twoCars.pmdb"))
+
     assert len(design.components) == 1
-    assert len(design.components[0].bodies) == 1
-    assert len(design.components[0].bodies[0].faces) == 6
+    assert len(design.components[0].components) == 12
+    assert len(design.components[0].components[0].components[0].bodies) == 1
+    assert len(design.components[0].components[0].components[0].bodies[0].faces) == 6
 
 
 def test_design_import_stride_with_named_selections(modeler: Modeler):
