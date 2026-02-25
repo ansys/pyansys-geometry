c04ae4ad3b2b09d0a9abd1c5957fc5e1d0f330d6
b-matteo <91963243+b-matteo@users.noreply.github.com>
2026-02-25 11:16:29 +0100

    Update test_spaceclaim_tutorial_examples.py

--- tests/integration/test_spaceclaim_tutorial_examples.py
+++ tests/integration/test_spaceclaim_tutorial_examples.py
@@ -411,9 +411,9 @@ def test_pull_example(modeler: Modeler):
     assert len(design.bodies) == 1
 
     assert body.volume.m == pytest.approx(
-        0.000995703006710242,
+        0.000995727877391032,
         rel=1e-6,
-        abs=1e-8,
+        abs=3e-8,
     )
 
 
