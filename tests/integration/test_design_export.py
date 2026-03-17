bf7447ea2a0e50cbf40bee13c6f264251934f342
RyanJWard <rward@synopsys.com>
2026-03-17 13:57:31 -0400

    fix error

--- tests/integration/test_design_export.py
+++ tests/integration/test_design_export.py
@@ -535,7 +535,7 @@ def test_import_export_open_file_design(
 
     # Parasolid vs SCDOCX can yield different nesting under the root component
     if file_format == DesignFileFormat.PARASOLID_TEXT:
-        if modeler.client.backend_version < (27, 1, 0)
+        if modeler.client.backend_version < (27, 1, 0):
             bodies = design.components[0].components[0].components[0].bodies
         else:
             # non-27.1: one less nesting level
