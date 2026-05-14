533c153d7ab6b39fb23dc8ca9ad789fdee40ca32
Ryan Ward <129954471+RyanJWard@users.noreply.github.com>
2026-05-14 09:04:27 -0400

    test: update PS nesting levels (#2771)
    Co-authored-by: pyansys-ci-bot <92810346+pyansys-ci-bot@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/integration/test_design_import.py
+++ tests/integration/test_design_import.py
@@ -529,12 +529,9 @@ def test_design_import_inventor2026(modeler: Modeler):
 def test_design_import_pmdb(modeler: Modeler):
     """Test importing a PMDB file."""
     # Open the design
-    design = modeler.open_file(Path(IMPORT_FILES_DIR, "PMDB/twoCars.pmdb"))
-
-    assert len(design.components) == 1
-    assert len(design.components[0].components) == 12
-    assert len(design.components[0].components[0].components[0].bodies) == 1
-    assert len(design.components[0].components[0].components[0].bodies[0].faces) == 6
+    design = modeler.open_file(Path(IMPORT_FILES_DIR, "PMDB/box.pmdb"))
+    assert len(design.bodies) == 1
+    assert len(design.bodies[0].faces) == 6
 
 
 def test_design_import_stride_with_named_selections(modeler: Modeler):
