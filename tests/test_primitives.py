f746ff1830cab1cafce7dc6fa12fcc7d0e6fb87a
Nicolas Gonzalez <niko04.gonzalez@gmail.com>
2026-08-05 16:39:18 -0400

    fix: Removed white space

--- tests/test_primitives.py
+++ tests/test_primitives.py
@@ -20,7 +20,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-
 from beartype.roar import BeartypeCallHintParamViolation
 import geomdl
 import numpy as np
