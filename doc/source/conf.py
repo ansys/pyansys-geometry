f5e43c848e75783ab808081d50188c26fa5906c7
Nicolas Gonzalez <35899950+Gonzi09@users.noreply.github.com>
2026-08-23 08:15:44 -0400

    docs: Document GeometryPlotter enhancement (#3062)
    Co-authored-by: pyansys-ci-bot <92810346+pyansys-ci-bot@users.noreply.github.com>
    Co-authored-by: Jacob Kerstetter <83893427+jacobrkerstetter@users.noreply.github.com>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -364,6 +364,7 @@ nbsphinx_thumbnails = {
     "examples/04_applied/04_mechanical_named_selections": "_static/thumbnails/mechanical_ns.png",
     "examples/04_applied/05_pmdb_export_options": "_static/thumbnails/pmdb_export_options.png",
     "examples/04_applied/06_solder_ball": "_static/thumbnails/solder_ball.png",
+    "examples/05_tools/selection_highlight": "_static/thumbnails/face_selection.png",
     "examples/05_tools/repair_tools": "_static/thumbnails/repair_tools.png",
     "examples/05_tools/prepare_tools": "_static/thumbnails/prepare_tools.png",
     "examples/05_tools/measurement_tools": "_static/thumbnails/measurement_tools.png",
