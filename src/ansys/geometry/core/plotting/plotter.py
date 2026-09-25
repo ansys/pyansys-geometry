a4ca8ea3ccf8cba1e878ddf20b2f7f6a66e0ac4b
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2026-09-25 19:39:44 +0200

    fix: use new dependenc version for viz interface

--- src/ansys/geometry/core/plotting/plotter.py
+++ src/ansys/geometry/core/plotting/plotter.py
@@ -682,12 +682,7 @@ class GeometryPlotter(PlotterInterface):
         """
         if plotting_object is not None:
             self.plot(plotting_object, **plotting_options)
-        theme = self._backend.pv_interface.scene.theme
-        original_edge_color = theme.edge_color
-        if self._backend._pl._show_plane:
-            theme.edge_color = "silver"
         picked_objs = self._backend.show(screenshot=screenshot, **plotting_options)
-        theme.edge_color = original_edge_color
 
         # Return the picked objects if picking is enabled... but as the actual PyAnsys
         # Geometry objects (or PyVista objects if not)
